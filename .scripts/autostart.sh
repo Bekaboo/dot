89ea770b3b138de039435e9b281038d354ebcd41
bekaboo <18127878294@qq.com>
2023-01-06 14:28:20 -0600

    Fix ~/.scripts/autostart.sh

--- .scripts/autostart.sh
+++ .scripts/autostart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # vim: ft=sh :
 
-if command -v &>/dev/null && command -v xdotool &>/dev/null
+if command -v fusuma &>/dev/null && command -v xdotool &>/dev/null
 then
     exec fusuma -d
 fi
