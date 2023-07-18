7e6f87f9c7891070ac43cedea465a1643bca36df
bekaboo <18127878294@qq.com>
2023-07-17 23:06:28 -0500

    Add fish true color themes & refactor fish config

---
+++ .config/fish/conf.d/cdls.fish
@@ -0,0 +1,5 @@
+function __dirchanged \
+        --on-variable PWD \
+        --description 'List directory contents after changing cwd'
+    ls --color=auto
+end
---
+++ .config/fish/conf.d/theme.fish
@@ -0,0 +1,23 @@
+function __term_supports_256color \
+        --description 'Check if terminal supports 256 color'
+    set -l colored_terms 256color wezterm alacritty kitty konsole yakuake
+    for term in $colored_terms
+        if string match -q --entire $term $TERM
+            return 0
+        end
+    end
+    return 1
+end
+
+function __fish_reload_theme \
+        --on-variable __fish_reload_theme \
+        --description 'Switch fish theme'
+    # echo =========================================================
+    # echo reload theme
+    # ls -l $__fish_config_dir/themes/Current.theme
+    __term_supports_256color;
+        and fish_config theme choose Current
+        or  fish_config theme choose 'Base Dark'
+end
+
+__fish_reload_theme
