af3e08c941b1c60a4b610412042834224312543d
bekaboo <18127878294@qq.com>
2023-07-19 02:01:16 -0500

    Enhence fish auto ls after cd: limit output size

--- .config/fish/conf.d/cdls.fish
+++ .config/fish/conf.d/cdls.fish
@@ -1,5 +1,14 @@
 function __dirchanged \
         --on-variable PWD \
         --description 'List directory contents after changing cwd'
-    ls --color=auto
+    set -l tcols (tput cols)
+    set -l max_lines (math ceil (tput lines) / 4)
+    set -l num_lines (count (ls -C --width=$tcols))
+    if test $num_lines -le $max_lines
+        ls --color=auto
+    else
+        ls -C --color --width=$tcols | head -n $max_lines
+        echo
+        echo ... $num_lines lines total
+    end
 end
