7731afcf0daaf7bdefd33524b0aa8fadf353e87b
bekaboo <18127878294@qq.com>
2023-07-19 01:26:25 -0500

    Add fish abbreviation to unalias commands

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -1,7 +1,15 @@
 function __command_abbr --description 'Add an command abbreviation'
     # $argv[1]: trigger
-    # $argv[2]: expansion
-    abbr --add $argv[1] --position command $argv[2]
+    abbr --add $argv[1] --position command $argv[2..-1]
+end
+
+function __unalias --description 'Unalias matching regex trigger'
+    # argv[1]: matched pattern
+    set -l command (string replace -r '^(\\\)(.+)' '$2' $argv[1])
+    if test "$command" = ' '
+        return 1
+    end
+    echo command $command
 end
 
 __command_abbr cp 'cp -i'
@@ -10,9 +18,10 @@ __command_abbr free 'free -m'
 __command_abbr lc 'wc -l'
 __command_abbr lzgit 'lazygit'
 __command_abbr mv 'mv -i'
-__command_abbr pip-install 'pip install --user'
-__command_abbr tree 'tree -m'
 __command_abbr nv 'nvim'
-__command_abbr v 'nvim'
+__command_abbr pip-install 'pip install --user'
 __command_abbr r 'ranger'
 __command_abbr sudo 'sudo -E'
+__command_abbr tree 'tree -m'
+__command_abbr unalias -r '\\\.+' -f __unalias
+__command_abbr v 'nvim'
