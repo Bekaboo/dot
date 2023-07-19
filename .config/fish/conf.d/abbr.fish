361948f80dfa21e1497ee35de6cc6a8dcfbdbe95
bekaboo <18127878294@qq.com>
2023-07-19 15:32:26 -0500

    Fix fish 'tree' abbreviation

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -22,6 +22,6 @@ __command_abbr nv 'nvim'
 __command_abbr pip-install 'pip install --user'
 __command_abbr r 'ranger'
 __command_abbr sudo 'sudo -E'
-__command_abbr tree 'tree -m'
+__command_abbr tree 'tree -N'
 __command_abbr unalias -r '\\\.+' -f __unalias
 __command_abbr v 'nvim'
