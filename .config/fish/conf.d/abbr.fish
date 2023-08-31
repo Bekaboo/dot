5453e8ebae0931d319ce3bb9b076be24661be12a
bekaboo <18127878294@qq.com>
2023-09-01 01:05:46 +0800

    Add fish abbr: 'dc' -> 'cd'

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -5,6 +5,7 @@ end
 
 __command_abbr cmd         'command'
 __command_abbr cp          'cp -i'
+__command_abbr dc          'cd'
 __command_abbr df          'df -h'
 __command_abbr fd          'fd -H -L --mount'
 __command_abbr free        'free -m'
