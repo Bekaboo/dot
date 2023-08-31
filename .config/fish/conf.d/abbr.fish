6c77ffec67e564c9789478d59b209098c3593e39
bekaboo <18127878294@qq.com>
2023-08-31 23:04:28 +0800

    Add fish abbr for fd

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -3,8 +3,10 @@ function __command_abbr --description 'Add an command abbreviation'
     abbr --add $argv[1] --position command $argv[2..-1]
 end
 
+__command_abbr cmd         'command'
 __command_abbr cp          'cp -i'
 __command_abbr df          'df -h'
+__command_abbr fd          'fd -H -L --mount'
 __command_abbr free        'free -m'
 __command_abbr g           'git'
 __command_abbr lc          'wc -l'
@@ -16,5 +18,4 @@ __command_abbr pip-install 'pip install --user'
 __command_abbr r           'ranger'
 __command_abbr sudo        'sudo -E'
 __command_abbr tree        'tree -N'
-__command_abbr cmd         'command'
 __command_abbr v           'nvim'
