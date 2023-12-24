7976af35ca82334163e9df88670f7f36d5eeba46
bekaboo <18127878294@qq.com>
2023-12-25 01:41:55 +0800

    Use $DISPLAY to test if has modern ui in fish shell

--- .config/fish/conf.d/theme.fish
+++ .config/fish/conf.d/theme.fish
@@ -1,21 +1,7 @@
-function __term_supports_256color \
-        --description 'Check if terminal supports 256 color'
-    if test "$COLORTERM" = 'truecolor'
-        return 0
-    end
-    set -l colored_terms 256color wezterm alacritty kitty konsole yakuake
-    for term in $colored_terms
-        if string match -q --entire $term $TERM
-            return 0
-        end
-    end
-    return 1
-end
-
 function __fish_reload_theme \
         --on-variable __fish_reload_theme \
         --description 'Switch fish theme'
-    __term_supports_256color;
+    test -n "$DISPLAY";
         and test -f "$__fish_config_dir/themes/Current.theme"
         and fish_config theme choose 'Current' 2> /dev/null
         or  fish_config theme choose 'Base Dark' 2> /dev/null
