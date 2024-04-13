8c790769d4948842eee4053b0af2ffe1a1977a12
bekaboo <18127878294@qq.com>
2024-04-13 19:40:08 +0800

    feat(fish): don't add extra newline after command 'clear'

--- .config/fish/conf.d/postexec.fish
+++ .config/fish/conf.d/postexec.fish
@@ -1,6 +1,12 @@
 # Source:
 # https://stackoverflow.com/questions/65722822/fish-shell-add-newline-before-prompt-only-when-previous-output-exists
-function postexec_test --on-event fish_postexec \
+function postexec_apppend_newline --on-event fish_postexec \
     --description 'Add newline before prompt only when previous output exists'
-   echo
+    # Don't add extra newline if the commandline is 'clear',
+    # The commandline is passed as the first parameter,
+    # see https://fishshell.com/docs/current/language.html#event
+    if string match -aqr $argv[1] '^\\s*clear\\s*\$'
+        return
+    end
+    echo
 end
