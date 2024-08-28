d7fb6519b45fedabdfecdc683b8ca2f33f819561
bekaboo <18127878294@qq.com>
2024-08-27 22:41:05 -0400

    feat(utils): term: add crontab as TUI app

--- lua/utils/term.lua
+++ lua/utils/term.lua
@@ -5,7 +5,7 @@ M.TUI_REGEX = vim.regex(
   [[\v^(sudo(\s+--?(\w|-)+((\s+|\=)\S+)?)*\s+)?]]
     .. [[(/usr/bin/)?]]
     .. [[(n?vim?|vimdiff|emacs(client)?|lem|nano]]
-    .. [[|helix|kak|tmux|lazygit|h?top|gdb|fzf|nmtui|sudoedit|ssh)]]
+    .. [[|helix|kak|tmux|lazygit|h?top|gdb|fzf|nmtui|sudoedit|ssh|crontab)]]
 )
 
 ---Check if any of the processes in terminal buffer `buf` is a TUI app
