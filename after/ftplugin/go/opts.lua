30e1eda5f48de0b7569df67d559d2d8bf1201e3e
bekaboo <18127878294@qq.com>
2024-09-03 17:46:12 -0400

    tweak(ftplugin): go: set 'formatoptions' to align with other languages

--- after/ftplugin/go/opts.lua
+++ after/ftplugin/go/opts.lua
@@ -2,3 +2,7 @@
 vim.bo.expandtab = false
 vim.bo.shiftwidth = 0
 vim.bo.softtabstop = 0
+
+-- Set 'formatoptions' to align with the behavior in other languages
+vim.opt_local.formatoptions:remove('t')
+vim.opt_local.formatoptions:append('croql')
