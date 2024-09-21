a5658cebd108f75c74429d56bc63ca69a344a9ac
bekaboo <18127878294@qq.com>
2024-09-21 10:58:12 -0400

    tweak(ftplugin): markdown & text: remove redundant 'linebreak' settings

--- after/ftplugin/markdown/opts.lua
+++ after/ftplugin/markdown/opts.lua
@@ -4,4 +4,3 @@ vim.bo.smartindent = false
 vim.bo.commentstring = '<!-- %s -->'
 
 vim.opt_local.wrap = true
-vim.opt_local.linebreak = true
--- after/ftplugin/text.lua
+++ after/ftplugin/text.lua
@@ -3,4 +3,3 @@ vim.bo.smartindent = false
 vim.bo.commentstring = '# %s'
 
 vim.opt_local.wrap = true
-vim.opt_local.linebreak = true
