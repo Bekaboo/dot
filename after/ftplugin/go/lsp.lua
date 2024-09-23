1e3943ba6045a208188efaef976a3ce054882aeb
bekaboo <18127878294@qq.com>
2024-09-23 01:21:46 -0400

    feat(ftplugin): go: add some options for gopls

--- after/ftplugin/go/lsp.lua
+++ after/ftplugin/go/lsp.lua
@@ -1,4 +1,23 @@
 require('utils.lsp').start({
   cmd = { 'gopls' },
   root_patterns = { 'go.work', 'go.mod' },
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+        shadow = true,
+      },
+      staticcheck = true,
+      hints = {
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        constantValues = true,
+        functionTypeParameters = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      }
+    },
+  },
 })
