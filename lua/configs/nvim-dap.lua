d67fabc1c2e0c65db2e15bac9deaf163c81bef40
bekaboo <18127878294@qq.com>
2024-09-23 08:22:37 -0400

    feat(configs): nvim-dap: use `<CR>` to repeat last dap function

--- lua/configs/nvim-dap.lua
+++ lua/configs/nvim-dap.lua
@@ -1,5 +1,6 @@
 local dap = require('dap')
-local static = require('utils.static')
+local keymap = require('utils.keymap')
+local icons = require('utils.static.icons')
 
 local function set_cond_breakpoint()
   dap.set_breakpoint(nil, nil, vim.fn.input('Breakpoint condition: '))
@@ -9,48 +10,83 @@ local function set_logpoint()
   dap.set_breakpoint(nil, nil, nil, vim.fn.input('Log point message: '))
 end
 
-vim.keymap.set('n', '<F1>', dap.up, { noremap = true })
-vim.keymap.set('n', '<F2>', dap.down, { noremap = true })
-vim.keymap.set('n', '<F5>', dap.continue, { noremap = true })
-vim.keymap.set('n', '<F6>', dap.pause, { noremap = true })
-vim.keymap.set('n', '<F8>', dap.repl.open, { noremap = true })
-vim.keymap.set('n', '<F9>', dap.toggle_breakpoint, { noremap = true })
-vim.keymap.set('n', '<F10>', dap.step_over, { noremap = true })
-vim.keymap.set('n', '<F11>', dap.step_into, { noremap = true })
-vim.keymap.set('n', '<F17>', dap.terminate, { noremap = true })
-vim.keymap.set('n', '<F23>', dap.step_out, { noremap = true })
-vim.keymap.set('n', '<F41>', dap.restart, { noremap = true })
-vim.keymap.set('n', '<F21>', set_cond_breakpoint, { noremap = true }) -- <S-F9>
-vim.keymap.set('n', '<F45>', set_logpoint, { noremap = true }) -- <C-S-F9>
+local last_dap_fn = function() end
 
-vim.keymap.set('n', '<Leader>Gk', dap.up, { noremap = true })
-vim.keymap.set('n', '<Leader>Gj', dap.down, { noremap = true })
-vim.keymap.set('n', '<Leader>G<Up>', dap.up, { noremap = true })
-vim.keymap.set('n', '<Leader>G<Down>', dap.down, { noremap = true })
-vim.keymap.set('n', '<Leader>Gc', dap.continue, { noremap = true })
-vim.keymap.set('n', '<Leader>Gh', dap.pause, { noremap = true })
-vim.keymap.set('n', '<Leader>Gp', dap.pause, { noremap = true })
-vim.keymap.set('n', '<C-c>', dap.pause, { noremap = true })
-vim.keymap.set('n', '<Leader>Ge', dap.repl.open, { noremap = true })
-vim.keymap.set('n', '<Leader>Gb', dap.toggle_breakpoint, { noremap = true })
-vim.keymap.set('n', '<Leader>Gn', dap.step_over, { noremap = true })
-vim.keymap.set('n', '<Leader>Gi', dap.step_into, { noremap = true })
-vim.keymap.set('n', '<Leader>Go', dap.step_out, { noremap = true })
-vim.keymap.set('n', '<Leader>Gt', dap.terminate, { noremap = true })
-vim.keymap.set('n', '<Leader>Gr', dap.restart, { noremap = true })
-vim.keymap.set('n', '<Leader>GB', set_cond_breakpoint, { noremap = true })
-vim.keymap.set('n', '<Leader>Gl', set_logpoint, { noremap = true })
+---Wrap a function to set it as the last function to be called
+---@param fn function
+---@return function
+local function wrap(fn)
+  return function()
+    last_dap_fn = fn
+    fn()
+  end
+end
+
+local w_set_cond_breakpoint = wrap(set_cond_breakpoint)
+local w_set_logpoint = wrap(set_logpoint)
+local w_up = wrap(dap.up)
+local w_down = wrap(dap.down)
+local w_continue = wrap(dap.continue)
+local w_pause = wrap(dap.pause)
+local w_repl_open = wrap(dap.repl.open)
+local w_toggle_breakpoint = wrap(dap.toggle_breakpoint)
+local w_step_over = wrap(dap.step_over)
+local w_step_into = wrap(dap.step_into)
+local w_step_out = wrap(dap.step_out)
+local w_terminate = wrap(dap.terminate)
+local w_restart = wrap(dap.restart)
+
+vim.keymap.set('n', '<F1>', w_up)
+vim.keymap.set('n', '<F2>', w_down)
+vim.keymap.set('n', '<F5>', w_continue)
+vim.keymap.set('n', '<F6>', w_pause)
+vim.keymap.set('n', '<F8>', w_repl_open)
+vim.keymap.set('n', '<F9>', w_toggle_breakpoint)
+vim.keymap.set('n', '<F10>', w_step_over)
+vim.keymap.set('n', '<F11>', w_step_into)
+vim.keymap.set('n', '<F17>', w_terminate)
+vim.keymap.set('n', '<F23>', w_step_out)
+vim.keymap.set('n', '<F41>', w_restart)
+vim.keymap.set('n', '<F21>', w_set_cond_breakpoint) -- <S-F9>
+vim.keymap.set('n', '<F45>', w_set_logpoint) -- <C-S-F9>
+
+vim.keymap.set('n', '<Leader>Gk', w_up)
+vim.keymap.set('n', '<Leader>Gj', w_down)
+vim.keymap.set('n', '<Leader>G<Up>', w_up)
+vim.keymap.set('n', '<Leader>G<Down>', w_down)
+vim.keymap.set('n', '<Leader>Gc', w_continue)
+vim.keymap.set('n', '<Leader>Gh', w_pause)
+vim.keymap.set('n', '<Leader>Gp', w_pause)
+vim.keymap.set('n', '<C-c>', w_pause)
+vim.keymap.set('n', '<Leader>Ge', w_repl_open)
+vim.keymap.set('n', '<Leader>Gb', w_toggle_breakpoint)
+vim.keymap.set('n', '<Leader>Gn', w_step_over)
+vim.keymap.set('n', '<Leader>Gi', w_step_into)
+vim.keymap.set('n', '<Leader>Go', w_step_out)
+vim.keymap.set('n', '<Leader>Gt', w_terminate)
+vim.keymap.set('n', '<Leader>Gr', w_restart)
+vim.keymap.set('n', '<Leader>GB', w_set_cond_breakpoint)
+vim.keymap.set('n', '<Leader>Gl', w_set_logpoint)
+
+-- When there's active dap session, use `<CR>` to repeat the last dap function
+keymap.amend('n', '<CR>', function(fallback)
+  if dap.session() then
+    last_dap_fn()
+    return
+  end
+  fallback()
+end)
 
 vim.api.nvim_create_user_command('DapClear', dap.clear_breakpoints, {
   desc = 'Clear all breakpoints',
 })
 
 -- stylua: ignore start
-vim.fn.sign_define('DapBreakpoint',          { text = vim.trim(static.icons.DotLarge), texthl = 'DiagnosticSignHint' })
-vim.fn.sign_define('DapBreakpointCondition', { text = vim.trim(static.icons.Diamond), texthl = 'DiagnosticSignInfo' })
-vim.fn.sign_define('DapBreakpointRejected',  { text = vim.trim(static.icons.DotLarge), texthl = 'DiagnosticSignWarn' })
-vim.fn.sign_define('DapLogPoint',            { text = vim.trim(static.icons.Log), texthl = 'DiagnosticSignOk' })
-vim.fn.sign_define('DapStopped',             { text = vim.trim(static.icons.ArrowRight), texthl = 'DiagnosticSignError' })
+vim.fn.sign_define('DapBreakpoint',          { text = vim.trim(icons.DotLarge), texthl = 'DiagnosticSignHint' })
+vim.fn.sign_define('DapBreakpointCondition', { text = vim.trim(icons.Diamond), texthl = 'DiagnosticSignInfo' })
+vim.fn.sign_define('DapBreakpointRejected',  { text = vim.trim(icons.DotLarge), texthl = 'DiagnosticSignWarn' })
+vim.fn.sign_define('DapLogPoint',            { text = vim.trim(icons.Log), texthl = 'DiagnosticSignOk' })
+vim.fn.sign_define('DapStopped',             { text = vim.trim(icons.ArrowRight), texthl = 'DiagnosticSignError' })
 -- stylua: ignore end
 
 dap.adapters = {}
