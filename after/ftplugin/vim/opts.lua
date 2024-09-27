2d250750bd56cdcc2752c461c0b1b8aeb4554c0d
bekaboo <18127878294@qq.com>
2024-09-26 21:09:49 -0400

    fix(ftplugin): vim: 'signcolumn' disabled in normal vim files

--- after/ftplugin/vim/opts.lua
+++ after/ftplugin/vim/opts.lua
@@ -1,6 +1,8 @@
 vim.bo.commentstring = '" %s'
 
-vim.bo.buflisted = false
-vim.opt_local.rnu = false
-vim.opt_local.signcolumn = 'no'
-vim.opt_local.statuscolumn = ''
+if vim.fn.win_gettype() == 'command' then
+  vim.bo.buflisted = false
+  vim.opt_local.rnu = false
+  vim.opt_local.signcolumn = 'no'
+  vim.opt_local.statuscolumn = ''
+end
