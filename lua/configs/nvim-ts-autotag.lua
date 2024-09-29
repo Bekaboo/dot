c55cc4c0287991b8d95cf39beea37164cf6825f0
bekaboo <18127878294@qq.com>
2024-09-29 18:21:49 -0400

    fix(configs): nvim-ts-autotag: ensure html parser is installed

--- lua/configs/nvim-ts-autotag.lua
+++ lua/configs/nvim-ts-autotag.lua
@@ -1,1 +1,11 @@
 require('nvim-ts-autotag').setup()
+
+local ts_ok, ts_configs = pcall(require, 'nvim-treesitter.configs')
+if ts_ok then
+  ---@diagnostic disable-next-line: missing-fields
+  ts_configs.setup({
+    ensure_installed = {
+      'html',
+    },
+  })
+end
