1c303fa3ea64a9f36aca1f61f46a52cac1c99b9b
bekaboo <18127878294@qq.com>
2024-09-29 18:08:10 -0400

    refactor(configs): LuaSnip: simplify config

--- lua/configs/LuaSnip.lua
+++ lua/configs/LuaSnip.lua
@@ -2,43 +2,6 @@ local ls = require('luasnip')
 local ls_types = require('luasnip.util.types')
 local static = require('utils.static')
 
----Load snippets for a given filetype
----@param ft string
----@return nil
-local function load_snippets(ft)
-  local ok, snip_groups = pcall(require, 'snippets.' .. ft)
-  if ok and type(snip_groups) == 'table' then
-    for _, snip_group in pairs(snip_groups) do
-      ls.add_snippets(ft, snip_group.snip or snip_group, snip_group.opts or {})
-    end
-  end
-end
-
----Load snippets lazily for different filetypes
----@return nil
-local function lazy_load_snippets()
-  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-    load_snippets(vim.bo[buf].ft)
-  end
-  vim.api.nvim_create_autocmd('FileType', {
-    group = vim.api.nvim_create_augroup('LuaSnipLazyLoadSnippets', {}),
-    desc = 'Lazy load snippets for different filetypes.',
-    callback = function(info)
-      load_snippets(vim.bo[info.buf].ft)
-    end,
-  })
-end
-
----@return nil
-local function set_keymaps()
-  -- stylua: ignore start
-  vim.keymap.set('s', '<Tab>',   function() ls.jump(1) end)
-  vim.keymap.set('s', '<S-Tab>', function() ls.jump(-1) end)
-  vim.keymap.set('s', '<C-n>',   function() return ls.choice_active() and '<Plug>luasnip-next-choice' or '<C-n>' end, { expr = true })
-  vim.keymap.set('s', '<C-p>',   function() return ls.choice_active() and '<Plug>luasnip-prev-choice' or '<C-p>' end, { expr = true })
-  -- stylua: ignore end
-end
-
 ls.setup({
   keep_roots = true,
   link_roots = false,
@@ -68,5 +31,33 @@ ls.setup({
   },
 })
 
-lazy_load_snippets()
-set_keymaps()
+---Load snippets for a given filetype
+---@param ft string
+---@return nil
+local function load_snippets(ft)
+  local ok, snip_groups = pcall(require, 'snippets.' .. ft)
+  if ok and type(snip_groups) == 'table' then
+    for _, snip_group in pairs(snip_groups) do
+      ls.add_snippets(ft, snip_group.snip or snip_group, snip_group.opts or {})
+    end
+  end
+end
+
+-- Lazy-load snippets based on filetype
+for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+  load_snippets(vim.bo[buf].ft)
+end
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('LuaSnipLazyLoadSnippets', {}),
+  desc = 'Lazy load snippets for different filetypes.',
+  callback = function(info)
+    load_snippets(vim.bo[info.buf].ft)
+  end,
+})
+
+-- stylua: ignore start
+vim.keymap.set('s', '<Tab>',   function() ls.jump(1) end)
+vim.keymap.set('s', '<S-Tab>', function() ls.jump(-1) end)
+vim.keymap.set('s', '<C-n>',   function() return ls.choice_active() and '<Plug>luasnip-next-choice' or '<C-n>' end, { expr = true })
+vim.keymap.set('s', '<C-p>',   function() return ls.choice_active() and '<Plug>luasnip-prev-choice' or '<C-p>' end, { expr = true })
+-- stylua: ignore end
