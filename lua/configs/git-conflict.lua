c5ea9853ca7ba5e69ca8d905d80d89dfa908a19a
bekaboo <18127878294@qq.com>
2024-10-05 02:22:00 -0400

    refactor(configs): git-conflict: change keymaps to choose both/none

--- lua/configs/git-conflict.lua
+++ lua/configs/git-conflict.lua
@@ -3,8 +3,8 @@ require('git-conflict').setup({
   default_mappings = {
     ours = 'c<',
     theirs = 'c>',
-    none = 'co',
-    both = 'c.',
+    none = 'c-',
+    both = 'c=',
     next = ']x',
     prev = '[x',
   },
