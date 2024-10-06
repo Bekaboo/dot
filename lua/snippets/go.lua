e54416275d38e7e29cc481f4515bdef9a3affae7
bekaboo <18127878294@qq.com>
2024-10-06 02:26:07 -0400

    fix(snippets): go: remove redundant jump nodes

--- lua/snippets/go.lua
+++ lua/snippets/go.lua
@@ -151,13 +151,12 @@ M.snippets = {
       { trig = 'pcheck' },
       common = { desc = 'Check a value of a variable through fmt.Println()' },
     },
-    un.fmtad('fmt.Println("<expr_escaped>:", <expr><e>)', {
+    un.fmtad('fmt.Println("<expr_escaped>:", <expr>)', {
       expr = i(1),
       expr_escaped = d(2, function(texts)
         local str = vim.fn.escape(texts[1][1], '\\"')
         return sn(nil, i(1, str))
       end, { 1 }),
-      e = i(3),
     })
   ),
   us.sn(
