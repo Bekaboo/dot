974fc7822b2e96faf870c685a0e04de4189278e2
bekaboo <18127878294@qq.com>
2024-10-08 03:50:37 -0400

    feat(ftplugin): python lsp: add pylint support

--- after/ftplugin/python/lsp.lua
+++ after/ftplugin/python/lsp.lua
@@ -8,8 +8,30 @@ local root_patterns = {
   'setup.py',
 }
 
+if vim.fn.executable('pylint') == 1 then
+  lsp.start({
+    cmd = { 'efm-langserver' },
+    name = 'efm-linter-pylint',
+    root_patterns = root_patterns,
+    settings = {
+      languages = {
+        python = {
+          {
+            lintSource = 'pylint',
+            lintCommand = 'pylint --score=no "${INPUT}"',
+            lintFormats = { '%f:%l:%c: %t%.%#: %m' },
+            lintStdin = false,
+            lintSeverity = 2,
+            rootMarkers = root_patterns,
+          },
+        },
+      },
+    },
+  })
+end
+
 -- Use efm to attach black formatter as a language server
-local efm = vim.fn.executable('black') == 1
+local formatter = vim.fn.executable('black') == 1
   and lsp.start({
     cmd = { 'efm-langserver' },
     root_patterns = root_patterns,
@@ -30,7 +52,7 @@ local efm = vim.fn.executable('black') == 1
 ---Disable lsp formatting capabilities if efm launched successfully
 ---@type fun(client: vim.lsp.Client, bufnr: integer)?
 local on_attach
-if efm then
+if formatter then
   function on_attach(client)
     client.server_capabilities.documentFormattingProvider = false
   end
