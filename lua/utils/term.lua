8d0e75bb4ddbb365a46d255c3be0d40248616c6d
bekaboo <18127878294@qq.com>
2024-10-10 00:13:39 -0400

    feat(utils): term: add asciinema as TUI app

--- lua/utils/term.lua
+++ lua/utils/term.lua
@@ -4,8 +4,8 @@ local M = {}
 M.TUI_REGEX = vim.regex(
   [[\v^(sudo(\s+--?(\w|-)+((\s+|\=)\S+)?)*\s+)?]]
     .. [[(/usr/bin/)?]]
-    .. [[(n?vim?|vimdiff|emacs(client)?|lem|nano]]
-    .. [[|helix|kak|tmux|lazygit|h?top|gdb|fzf|nmtui|sudoedit|ssh|crontab)]]
+    .. [[(n?vim?|vimdiff|emacs(client)?|lem|nano|helix|kak|]]
+    .. [[tmux|lazygit|h?top|gdb|fzf|nmtui|sudoedit|ssh|crontab|asciinema)]]
 )
 
 ---Check if any of the processes in terminal buffer `buf` is a TUI app
