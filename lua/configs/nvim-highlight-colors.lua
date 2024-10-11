da3add50c2f3c6a53cc3296faed1d8566cf1898e
bekaboo <18127878294@qq.com>
2024-10-11 03:39:27 -0400

    fix(configs): nvim-highlight-colors: hl on 2nd win of buf not updated

--- lua/configs/nvim-highlight-colors.lua
+++ lua/configs/nvim-highlight-colors.lua
@@ -18,49 +18,52 @@ local _highlight_colors = nhc.highlight_colors
 ---Hijack original `highlight_colors()` to apply highlighting only to unfolded
 ---lines, significantly improving performance when large folds are present in
 ---visible range
----@param min_row number
----@param max_row number
 ---@param buf number
 ---@return nil
-function nhc.highlight_colors(min_row, max_row, buf)
+function nhc.highlight_colors(_, _, buf)
   if not vim.api.nvim_buf_is_valid(buf) then
     return
   end
 
-  local win = vim.fn.bufwinid(buf)
-  if win == -1 then
-    return -- buffer not visible
-  end
+  for _, win in ipairs(vim.fn.win_findbuf(buf)) do
+    local visible_start = vim.api.nvim_win_call(win, function()
+      return vim.fn.line('w0')
+    end)
+    local visible_end = vim.api.nvim_win_call(win, function()
+      return vim.fn.line('w$')
+    end)
 
-  local wp = ffi.C.find_window_by_handle(win, ffi.new('Error'))
-  if not vim.wo[win].foldenable or not ffi.C.hasAnyFolding(wp) then
-    _highlight_colors(min_row, max_row, buf)
-    return
-  end
-
-  local unfolded_start, unfolded_end ---@type integer?
-  local fold_lastp = ffi.new('linenr_T[1]')
-  local linenr = min_row
-  while linenr <= max_row do
-    if not ffi.C.hasFolding(wp, linenr, nil, fold_lastp) then
-      unfolded_start = unfolded_start or linenr
-      unfolded_end = linenr
-      linenr = linenr + 1
+    local wp = ffi.C.find_window_by_handle(win, ffi.new('Error'))
+    if not vim.wo[win].foldenable or not ffi.C.hasAnyFolding(wp) then
+      _highlight_colors(visible_start, visible_end, buf)
       goto continue
     end
 
-    -- If has folding at `linenr`, next unfolded (visible) line
-    -- must at least be the next line of the last folded line
-    linenr = fold_lastp[0] + 1
+    local unfolded_start, unfolded_end ---@type integer?
+    local fold_lastp = ffi.new('linenr_T[1]')
+    local linenr = visible_start
+    while linenr <= visible_end do
+      if not ffi.C.hasFolding(wp, linenr, nil, fold_lastp) then
+        unfolded_start = unfolded_start or linenr
+        unfolded_end = linenr
+        linenr = linenr + 1
+        goto continue
+      end
+
+      -- If has folding at `linenr`, next unfolded (visible) line
+      -- must at least be the next line of the last folded line
+      linenr = fold_lastp[0] + 1
+      if unfolded_start and unfolded_end then
+        _highlight_colors(unfolded_start, unfolded_end, buf)
+        unfolded_start, unfolded_end = nil, nil
+      end
+      ::continue::
+    end
+
     if unfolded_start and unfolded_end then
       _highlight_colors(unfolded_start, unfolded_end, buf)
-      unfolded_start, unfolded_end = nil, nil
     end
     ::continue::
-  end
-
-  if unfolded_start and unfolded_end then
-    _highlight_colors(unfolded_start, unfolded_end, buf)
   end
 end
 
