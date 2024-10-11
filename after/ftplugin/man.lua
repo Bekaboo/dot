5677d8ce7288f04c6b4b0f88b9618378a839a68b
bekaboo <18127878294@qq.com>
2024-10-11 19:18:19 -0400

    refactor(ftplugin): man,help: remove keymap 'q' in man/help pages

--- after/ftplugin/man.lua
+++ after/ftplugin/man.lua
@@ -11,6 +11,5 @@ vim.opt_local.signcolumn = 'no'
 vim.opt_local.spell = false
 vim.opt_local.statuscolumn = ''
 
-vim.keymap.set({ 'n', 'x' }, 'q', 'ZQ', { buffer = true, nowait = true })
 vim.keymap.set({ 'n', 'x' }, 'd', '<C-d>', { buffer = true, nowait = true })
 vim.keymap.set({ 'n', 'x' }, 'u', '<C-u>', { buffer = true, nowait = true })
