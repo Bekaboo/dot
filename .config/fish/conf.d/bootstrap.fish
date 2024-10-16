76a81987b43c6cd6ecb470e11aea95dcf1e88734
bekaboo <18127878294@qq.com>
2024-10-16 13:45:53 -0700

    fix(fish): duplicate paths in `$fish_{function,complete}_path`

--- .config/fish/conf.d/bootstrap.fish
+++ .config/fish/conf.d/bootstrap.fish
@@ -11,10 +11,20 @@ end
 set -l data_home (test -n "$XDG_DATA_HOME";
                   and echo "$XDG_DATA_HOME";
                   or echo "$HOME/.local/share")
-set -Ux fisher_path "$data_home/fish/plugin"
+set -gx fisher_path "$data_home/fish/plugin"
 
-set -Ux fish_function_path $fish_function_path[1] "$fisher_path/functions" $fish_function_path[2..-1]
-set -Ux fish_complete_path $fish_complete_path[1] "$fisher_path/completions" $fish_complete_path[2..-1]
+# Don't use `set -gx` here as lists as environment variables
+# (variable set with `-x`) will collapse to strings when exported to subshell
+if not contains "$fisher_path/functions" $fish_function_path
+    set -g fish_function_path $fish_function_path[1] \
+        "$fisher_path/functions" $fish_function_path[2..-1]
+end
+
+if not contains "$fisher_path/completions" $fish_complete_path
+    set -g fish_complete_path $fish_complete_path[1] \
+        "$fisher_path/completions" $fish_complete_path[2..-1]
+end
+
 for file in $fisher_path/conf.d/*.fish
     source $file
 end
