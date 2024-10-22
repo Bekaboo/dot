be744b93f8990ac745a0ab0e803f01ba47a701d6
bekaboo <18127878294@qq.com>
2024-10-22 04:18:05 -0400

    chore(fish): postexec: add underscores for non-interactive funcs; fix typo

--- .config/fish/conf.d/postexec.fish
+++ .config/fish/conf.d/postexec.fish
@@ -4,7 +4,7 @@ end
 
 # Source:
 # https://stackoverflow.com/questions/65722822/fish-shell-add-newline-before-prompt-only-when-previous-output-exists
-function postexec_apppend_newline --on-event fish_postexec \
+function __postexec_append_newline --on-event fish_postexec \
     --description 'Add newline before prompt only when previous output exists'
     # Don't add extra newline if the commandline is 'clear',
     # The commandline is passed as the first parameter,
