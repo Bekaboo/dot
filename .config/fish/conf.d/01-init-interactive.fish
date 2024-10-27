33b556d11dc89d58e8e7e47a6607f15674d3af28
bekaboo <18127878294@qq.com>
2024-10-27 02:45:57 -0400

    feat(fish): prettier python venv prompt

--- .config/fish/conf.d/01-init-interactive.fish
+++ .config/fish/conf.d/01-init-interactive.fish
@@ -127,3 +127,9 @@ end
 # Ensure color theme files are correctly linked
 type -q setbg; and setbg
 type -q setcolors; and setcolors
+
+# Disable venv prompt provided by `activate.fish` -- we handle it ourselves in
+# `functions/fish_right_prompt.fish`
+# Also, shouldn't export this variable with `set -gx` as we don't want to to
+# affect other shells, e.g. `bash` that is nested in `fish`
+set -g VIRTUAL_ENV_DISABLE_PROMPT true
--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,6 +1,17 @@
+function __fish_venv_prompt
+    if test -n "$VIRTUAL_ENV"
+        echo basename $VIRTUAL_ENV
+    end
+end
+
 function fish_right_prompt --description 'Write out the right prompt'
-    echo -n -s \
-        (set_color $fish_color_vcs) \
-            (string replace -r '^(\s*\()(\w+)' '$1\#$2' \
-                (fish_vcs_prompt))
+    set -l vcs_info (string match -gr '([^() ]+)' (fish_vcs_prompt))
+    set -l venv_info (test -n "$VIRTUAL_ENV"
+        and echo (basename $VIRTUAL_ENV)
+        or echo '')
+
+    set -l info (string join ', ' (string match -v '' $venv_info $vcs_info))
+    if test -n "$info"
+        echo -n -s (set_color $fish_color_vcs) '(' $info ')'
+    end
 end
