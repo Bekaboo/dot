30110dc27c90cdc93f1a0daf9f23e52afdcdd82c
bekaboo <kankefengjing@gmail.com>
2024-11-12 00:50:56 -0500

    refactor(nvim): don't set bg for markdown inline code

--- .config/nvim/lua/configs/headlines.lua
+++ .config/nvim/lua/configs/headlines.lua
@@ -15,13 +15,7 @@ require('headlines').setup({
 ---Set default highlight groups for headlines.nvim
 ---@return nil
 local function set_default_hlgroups()
-  local hl = require('utils.hl')
-  hl.set(0, 'CodeBlock', { bg = 'CursorLine' })
-  hl.set(0, 'markdownCode', { bg = 'CursorLine', fg = 'markdownCode' })
-  hl.set(0, '@markup.raw.markdown_inline', {
-    bg = 'CursorLine',
-    fg = 'markdownCode',
-  })
+  require('utils.hl').set(0, 'CodeBlock', { bg = 'CursorLine' })
 end
 set_default_hlgroups()
 
