e5e2b1077902c9f4ebc5a8b02ec47fe8f6b96823
bekaboo <kankefengjing@gmail.com>
2024-12-02 23:02:54 -0500

    refactor(nvim)!: remove vim-matchup
    Slow in large files e.g. C file with over 5000+ lines with treesitter
    enabled.

--- .config/nvim/lua/configs/vim-matchup.lua
+++
@@ -1,5 +0,0 @@
-vim.g.matchup_matchparen_deferred = 1 -- improve scrolling performance
-vim.g.matchup_matchparen_deferred_show_delay = 16
-vim.g.matchup_matchparen_deferred_hide_delay = 16
-vim.g.matchup_matchparen_offscreen = {} -- don't show offscreen matches
-vim.g.matchup_matchparen_end_sign = require('utils.static').icons.ArrowLeft
--- .config/nvim/lua/modules/edit.lua
+++ .config/nvim/lua/modules/edit.lua
@@ -41,27 +41,4 @@ return {
       require('configs.vim-easy-align')
     end,
   },
-
-  {
-    'andymass/vim-matchup',
-    lazy = true,
-    init = function()
-      -- Disable matchit and matchparen
-      vim.g.loaded_matchparen = 0
-      vim.g.loaded_matchit = 0
-
-      -- Lazy-load after UIEnter
-      vim.api.nvim_create_autocmd('UIEnter', {
-        once = true,
-        callback = vim.schedule_wrap(function()
-          require('match-up').setup({ sync = true })
-          vim.fn['matchup#loader#init_buffer']()
-          return true
-        end),
-      })
-    end,
-    config = function()
-      require('configs.vim-matchup')
-    end,
-  },
 }
