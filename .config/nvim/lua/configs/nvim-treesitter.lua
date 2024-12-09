d4401cb5fa100ca2c35927284f1c681e9e2bb3c5
bekaboo <kankefengjing@gmail.com>
2024-12-09 03:54:58 -0500

    refactor(nvim): use built-in `vim.treesitter.foldexpr` instead of nvim-treesitter's

--- .config/nvim/lua/configs/nvim-treesitter.lua
+++ .config/nvim/lua/configs/nvim-treesitter.lua
@@ -1,39 +1,5 @@
 local ts_configs = require('nvim-treesitter.configs')
 
----@param buf integer
----@return nil
-local function enable_ts_folding(buf)
-  -- Treesitter folding is extremely slow in large files,
-  -- making typing and undo lag as hell
-  --
-  -- Also disable ts folding in markdown files because it
-  -- is so slooow compared to other filetypes
-  if
-    not vim.api.nvim_buf_is_valid(buf)
-    or vim.bo[buf].ft == 'markdown'
-    or vim.b[buf].bigfile
-  then
-    return
-  end
-  vim.api.nvim_buf_call(buf, function()
-    local o = vim.opt_local
-    local fdm = o.fdm:get() ---@diagnostic disable-line: undefined-field
-    local fde = o.fde:get() ---@diagnostic disable-line: undefined-field
-    o.fdm = fdm == 'manual' and 'expr' or fdm
-    o.fde = fde == '0' and 'nvim_treesitter#foldexpr()' or fde
-  end)
-end
-
-enable_ts_folding(0)
-
--- Set treesitter folds
-vim.api.nvim_create_autocmd('FileType', {
-  group = vim.api.nvim_create_augroup('TSFolds', {}),
-  callback = function(info)
-    enable_ts_folding(info.buf)
-  end,
-})
-
 ---@diagnostic disable-next-line: missing-fields
 ts_configs.setup({
   -- Make sure that we install all parsers shipped with neovim so that we don't
