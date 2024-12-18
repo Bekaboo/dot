8a50e24320c1373e633ed9f4402abf2d8f1cb36d
bekaboo <kankefengjing@gmail.com>
2024-12-17 22:17:29 -0500

    perf(nvim): lazy-load custom filetypes

--- .config/nvim/filetype.lua
+++ .config/nvim/filetype.lua
@@ -1,7 +1,13 @@
-vim.filetype.add({
-  extension = {
-    -- Special filetypes for Weixin Mini Program
-    wxss = 'css',
-    wxml = 'html',
-  },
+vim.api.nvim_create_autocmd('BufReadPre', {
+  once = true,
+  callback = function()
+    vim.filetype.add({
+      extension = {
+        -- Special filetypes for Weixin Mini Program
+        wxss = 'css',
+        wxml = 'html',
+      },
+    })
+    return true
+  end,
 })
