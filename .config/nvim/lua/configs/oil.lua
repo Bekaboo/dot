24f95bc3666f405cfa672e47087391ea23413f00
bekaboo <kankefengjing@gmail.com>
2024-12-19 07:53:42 -0500

    refactor(nvim): don't make oil buffer follow cwd

--- .config/nvim/lua/configs/oil.lua
+++ .config/nvim/lua/configs/oil.lua
@@ -431,7 +431,7 @@ oil.setup({
   },
 })
 
-local groupid = vim.api.nvim_create_augroup('OilSyncCwd', {})
+local groupid = vim.api.nvim_create_augroup('OilSetup', {})
 vim.api.nvim_create_autocmd('BufEnter', {
   desc = 'Ensure that oil buffers are not listed.',
   group = groupid,
@@ -455,21 +455,6 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'TextChanged' }, {
     end
   end,
 })
-vim.api.nvim_create_autocmd('DirChanged', {
-  desc = 'Let oil buffers follow cwd.',
-  group = groupid,
-  callback = function(info)
-    if vim.bo[info.buf].filetype == 'oil' then
-      vim.defer_fn(function()
-        local cwd = vim.fs.normalize(vim.fn.getcwd(vim.fn.winnr()))
-        local oildir = vim.fs.normalize(oil.get_current_dir() or '')
-        if cwd ~= oildir and vim.bo.ft == 'oil' then
-          oil.open(cwd)
-        end
-      end, 100)
-    end
-  end,
-})
 
 vim.api.nvim_create_autocmd('BufEnter', {
   desc = 'Record alternate file in dir buffers.',
@@ -485,7 +470,7 @@ vim.api.nvim_create_autocmd('BufEnter', {
 
 vim.api.nvim_create_autocmd('BufEnter', {
   desc = 'Set last cursor position in oil buffers when editing parent dir.',
-  group = vim.api.nvim_create_augroup('OilSetLastCursor', {}),
+  group = groupid,
   pattern = 'oil://*',
   callback = function()
     -- Place cursor on the alternate buffer if we are opening
