61f7d6bd121bf846f04eef056ba274dc6dff0f49
bekaboo <kankefengjing@gmail.com>
2024-12-21 03:52:36 -0500

    fix(nvim): plugin cmds undefined when launched with `nvim +<cmd>`

--- .config/nvim/lua/core/modules.lua
+++ .config/nvim/lua/core/modules.lua
@@ -138,18 +138,41 @@ local function enable_modules(module_names)
     end
   end
 
-  (vim.fn.argc(-1) == 0 and function(cb)
-    vim.api.nvim_create_autocmd('UIEnter', {
-      once = true,
-      callback = function()
-        require('lazy.stats').on_ui_enter()
-        vim.schedule(cb)
-        return true
+  ---@param cb function
+  ---@return nil
+  local function defer(cb)
+    cb()
+  end
+
+  if vim.fn.argc(-1) == 0 then
+    local missed_cmds = {}
+    local cmd_groupid = vim.api.nvim_create_autocmd('CmdUndefined', {
+      callback = function(info)
+        table.insert(missed_cmds, info.match)
       end,
     })
-  end or function(cb)
-    cb()
-  end)(function()
+
+    ---@param cb function
+    ---@return nil
+    defer = function(cb)
+      vim.api.nvim_create_autocmd('UIEnter', {
+        once = true,
+        callback = function()
+          require('lazy.stats').on_ui_enter()
+          vim.schedule(function()
+            cb()
+            vim.api.nvim_del_autocmd(cmd_groupid)
+            for _, cmd in ipairs(missed_cmds) do
+              vim.cmd(cmd)
+            end
+          end)
+          return true
+        end,
+      })
+    end
+  end
+
+  defer(function()
     require('lazy.view.config').keys.details = '='
     require('lazy').setup(specs, {
       root = vim.g.package_path,
