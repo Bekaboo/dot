993ea1826e2791ea8c05fa39a3c4efa83f1fbc00
bekaboo <kankefengjing@gmail.com>
2024-12-22 02:42:52 -0500

    perf(nvim): disable `vim.validate()`

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,7 @@
+-- Disable validation for speed
+---@diagnostic disable-next-line: duplicate-set-field
+vim.validate = function() end
+
 -- Enable faster lua loader using byte-compilation
 -- https://github.com/neovim/neovim/commit/2257ade3dc2daab5ee12d27807c0b3bcf103cd29
 vim.loader.enable()
