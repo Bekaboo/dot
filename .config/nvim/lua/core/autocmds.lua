a304692d769644a8c3bfbd4ec54fae5eac3cda17
bekaboo <kankefengjing@gmail.com>
2024-12-24 20:32:44 -0500

    fix(nvim): sometimes syntax enabled in big files

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -30,8 +30,7 @@ augroup('BigFileSettings', {
         vim.opt_local.signcolumn = 'no'
         vim.opt_local.foldcolumn = '0'
         vim.opt_local.winbar = ''
-        vim.opt_local.syntax = ''
-        autocmd('BufReadPost', {
+        autocmd('FileType', {
           once = true,
           buffer = info.buf,
           callback = function()
