e0d49603913dbcd3f6d94de004dad8a228fe935f
bekaboo <kankefengjing@gmail.com>
2025-01-20 17:39:46 -0500

    fix(nvim): table-mode should be enabled per-buffer

--- .config/nvim/lua/configs/vim-table-mode.lua
+++ .config/nvim/lua/configs/vim-table-mode.lua
@@ -1,15 +1,32 @@
 ---@param buf integer?
 local function table_mode_toggle(buf)
   buf = buf or vim.api.nvim_get_current_buf()
-  local ft = vim.bo[buf].ft
-  local cmd = (ft == 'markdown' or ft == 'text') and 'TableModeEnable'
-    or 'TableModeDisable'
-  vim.cmd[cmd]({
-    mods = {
-      silent = true,
-      emsg_silent = true,
-    },
-  })
+  if not vim.api.nvim_buf_is_valid(buf) then
+    return
+  end
+
+  vim.api.nvim_buf_call(buf, function()
+    local ft = vim.bo.ft
+    local table_mode_buf_is_active = vim.fn['tablemode#IsActive']() == 1
+    if ft == 'markdown' and not table_mode_buf_is_active then
+      vim.cmd.TableModeEnable({
+        mods = {
+          silent = true,
+          emsg_silent = true,
+        },
+      })
+      return
+    end
+
+    if ft ~= 'markdown' and table_mode_buf_is_active then
+      vim.cmd.TableModeDisable({
+        mods = {
+          silent = true,
+          emsg_silent = true,
+        },
+      })
+    end
+  end)
 end
 
 table_mode_toggle()
