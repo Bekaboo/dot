743ddcf87a42d9f28ab7a70e9d68563df1f9cfef
bekaboo <kankefengjing@gmail.com>
2025-01-20 19:33:17 -0500

    feat(nvim): make z plugin completion work for multiple args

--- .config/nvim/lua/plugin/z.lua
+++ .config/nvim/lua/plugin/z.lua
@@ -17,6 +17,23 @@ local function has_z()
   return false
 end
 
+---Given a list of args for `z`, return its corresponding escaped string to be
+---used as a shell command argument
+---@param args string[]?
+---@return string
+local function get_arg_str(args)
+  if not args then
+    return ''
+  end
+
+  return table.concat(
+    vim.tbl_map(function(path)
+      return vim.fn.shellescape(vim.fn.expand(path))
+    end, args),
+    ' '
+  )
+end
+
 ---Change directory to the most frequently visited directory using `z`
 ---@param input string[]
 function M.z(input)
@@ -24,28 +41,20 @@ function M.z(input)
     return
   end
 
-  local dest =
-    vim.trim(vim.fn.system('z -e ' .. table.concat(
-      vim.tbl_map(function(path)
-        return vim.fn.shellescape(vim.fn.expand(path))
-      end, input),
-      ' '
-    )))
+  local dest = vim.trim(vim.fn.system('z -e ' .. get_arg_str(input)))
   vim.cmd.lcd(vim.fn.fnameescape(dest))
 end
 
----Complete `:Z` command
----@param input string?
+---List matching z directories given input
+---@param input string[]?
 ---@return string[]
-function M.cmp(input)
+function M.list(input)
   if not has_z() then
     return {}
   end
 
-  local scored_paths =
-    vim.fn.systemlist('z -l ' .. vim.fn.shellescape(input or ''))
   local paths = {}
-  for _, candidate in ipairs(scored_paths) do
+  for _, candidate in ipairs(vim.fn.systemlist('z -l ' .. get_arg_str(input))) do
     local path = candidate:match('^[0-9.]+%s+(.*)') -- trim score
     if path then
       table.insert(paths, path)
@@ -54,14 +63,34 @@ function M.cmp(input)
   return paths
 end
 
+---Return a complete function for given z command
+---@param cmd string
+---@return function
+function M.cmp(cmd)
+  local cmd_reg = string.format('.*%s%%s+', cmd)
+  ---@param cmdline string the entire command line
+  ---@param cursorpos integer cursor position in the command line
+  ---@return string[] completion completion results
+  return function(_, cmdline, cursorpos)
+    -- HACK: use sting manipulation to get all args after the command instead
+    -- of using `arglead` (only the last argument before cursor) to make the
+    -- completion for multiple arguments just link `z` in shell
+    -- e.g. if paths `/foo/bar` and `/baz/bar` are in z's database, then
+    -- `z foo bar` should only complete `/foo/bar` instead of both (using
+    -- both 'foo' and 'bar' to match)
+    -- TODO: only split on spaces that is not escaped
+    return M.list(vim.split(cmdline:sub(1, cursorpos):gsub(cmd_reg, ''), ' '))
+  end
+end
+
 ---Select and jump to z directories using `vim.ui.select()`
----@param input string?
+---@param input string[]?
 function M.select(input)
   if not has_z() then
     return
   end
 
-  local paths = M.cmp(input)
+  local paths = M.list(input)
   vim.ui.select(paths, {
     prompt = 'Change cwd to: ',
   }, function(choice)
@@ -83,14 +112,14 @@ function M.setup()
   end, {
     nargs = '*',
     desc = 'Change local working directory using z.',
-    complete = M.cmp,
+    complete = M.cmp('Z'),
   })
   vim.api.nvim_create_user_command('ZSelect', function(args)
-    M.select(args.args)
+    M.select(args.fargs)
   end, {
     nargs = '*',
     desc = 'Pick from z directories with `vim.ui.select()`.',
-    complete = M.cmp,
+    complete = M.cmp('ZSelect'),
   })
 end
 
