918eb4fe8684520a3d3750af6580063eb334b9f2
bekaboo <kankefengjing@gmail.com>
2025-01-22 12:38:54 -0500

    feat(nvim): fallback to treesitter to detect math zone

--- .config/nvim/lua/utils/ft/tex.lua
+++ .config/nvim/lua/utils/ft/tex.lua
@@ -3,8 +3,15 @@ local M = {}
 ---Returns whether the cursor is in a math zone
 ---@return boolean
 function M.in_mathzone()
-  return vim.b.vimtex_id ~= nil
-    and vim.F.npcall(vim.api.nvim_eval, 'vimtex#syntax#in_mathzone()') == 1
+  if vim.b.vimtex_id then
+    return vim.F.npcall(vim.api.nvim_eval, 'vimtex#syntax#in_mathzone()') == 1
+  end
+
+  return require('utils.ts').in_node(function(type)
+    return type:match('formula')
+      or type:match('equation')
+      or type:match('math')
+  end, { ignore_injections = false })
 end
 
 ---Returns whether the cursor is in normal zone (not in math zone)
