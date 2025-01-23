628b339cfcb9a5577367e6dc99c4269740ecf242
bekaboo <kankefengjing@gmail.com>
2025-01-23 01:09:01 -0500

    perf(nvim): avoid calling `node:type()` type repeately in `utils.ts.ins_node()`

--- .config/nvim/lua/utils/ts.lua
+++ .config/nvim/lua/utils/ts.lua
@@ -32,10 +32,10 @@ function M.is_active(buf)
 end
 
 ---Returns whether cursor is in a specific type of treesitter node
----@param ntype string|string[]|fun(type: string): boolean type of node, or function to check node type
+---@param types string|string[]|fun(type: string): boolean type of node, or function to check node type
 ---@param opts vim.treesitter.get_node.Opts?
 ---@return boolean
-function M.in_node(ntype, opts)
+function M.in_node(types, opts)
   if not M.is_active(opts and opts.bufnr) then
     return false
   end
@@ -45,21 +45,17 @@ function M.in_node(ntype, opts)
     return false
   end
 
-  if vim.is_callable(ntype) then
-    return ntype(node:type())
+  local nt = node:type() -- current node type
+  if vim.is_callable(types) then
+    return types(nt)
   end
 
-  if type(ntype) == 'string' then
-    ntype = { ntype }
+  if type(types) == 'string' then
+    types = { types }
   end
-
-  for _, t in ipairs(ntype --[=[@as string[]]=]) do
-    if node:type():match(t) then
-      return true
-    end
-  end
-
-  return false
+  return vim.iter(types):any(function(t)
+    return nt:match(t)
+  end)
 end
 
 ---Get language at given buffer position, useful in files with injected syntax
