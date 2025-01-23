9c18377b723d0d58fbd5d2b861c6b75b71d7bdcd
bekaboo <kankefengjing@gmail.com>
2025-01-23 08:46:02 -0500

    feat(nvim): load clang_extensions.nvim for more filetypes

--- .config/nvim/lua/modules/langs.lua
+++ .config/nvim/lua/modules/langs.lua
@@ -1,7 +1,7 @@
 return {
   {
     'p00f/clangd_extensions.nvim',
-    ft = { 'c', 'cpp' },
+    ft = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
     config = function()
       require('configs.clangd_extensions')
     end,
