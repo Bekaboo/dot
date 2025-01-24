0be62551f2384bc19008fb62d39182c5b8f326e5
bekaboo <kankefengjing@gmail.com>
2025-01-24 15:15:34 -0500

    feat(nvim): fallback to `ccls` if `clangd` is not available

--- .config/nvim/after/ftplugin/c/lsp.lua
+++ .config/nvim/after/ftplugin/c/lsp.lua
@@ -1,11 +1,25 @@
-require('utils.lsp').start({
-  cmd = { 'clangd' },
-  root_patterns = {
-    '.clangd',
-    '.clang-tidy',
-    '.clang-format',
-    'compile_commands.json',
-    'compile_flags.txt',
-    'configure.ac',
+local lsp = require('utils.lsp')
+
+local server_configs = {
+  {
+    cmd = { 'clangd' },
+    root_patterns = {
+      '.clangd',
+      '.clang-tidy',
+      '.clang-format',
+      'compile_commands.json',
+      'compile_flags.txt',
+      'configure.ac',
+    },
   },
-})
+  {
+    cmd = { 'ccls' },
+    root_patterns = { '.ccls', 'compile_commands.json' },
+  },
+}
+
+for _, server_config in ipairs(server_configs) do
+  if lsp.start(server_config) then
+    return
+  end
+end
