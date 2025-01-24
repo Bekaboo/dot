be99fc41b7b75533389949709a82c4155f6bab07
bekaboo <kankefengjing@gmail.com>
2025-01-24 17:01:16 -0500

    feat(nvim): add `before_init` and commands for rust-analyzer

--- .config/nvim/after/ftplugin/rust/lsp.lua
+++ .config/nvim/after/ftplugin/rust/lsp.lua
@@ -16,4 +16,22 @@ require('utils.lsp').start({
       serverStatusNotification = true,
     },
   },
+  before_init = function(params, config)
+    -- See https://github.com/rust-lang/rust-analyzer/blob/eb5da56d839ae0a9e9f50774fa3eb78eb0964550/docs/dev/lsp-extensions.md?plain=1#L26
+    if config.settings then
+      params.initializationOptions = config.settings['rust-analyzer']
+    end
+  end,
+  -- Source: https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/configs/rust_analyzer.lua
+  on_attach = function(client)
+    vim.api.nvim_buf_create_user_command(0, 'CargoReload', function()
+      client.request('rust-analyzer/reloadWorkspace', nil, function(err)
+        if err then
+          vim.notify('Corresponding reload cargo workspace: ' .. tostring(err))
+          return
+        end
+        vim.notify('Cargo workspace reloaded')
+      end, 0)
+    end, { description = 'Reload current cargo workspace' })
+  end,
 })
