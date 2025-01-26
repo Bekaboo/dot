6a788591afd484e883eb1efba56d9dc5018aa431
bekaboo <kankefengjing@gmail.com>
2025-01-26 12:41:32 -0500

    feat(nvim): support basedpyright lsp

--- .config/nvim/after/ftplugin/python/lsp.lua
+++ .config/nvim/after/ftplugin/python/lsp.lua
@@ -57,59 +57,93 @@ if formatter then
   end
 end
 
+local pyright_root_patterns =
+  vim.list_extend({ 'pyrightconfig.json' }, root_patterns)
+
+local pyright_settings = {
+  analysis = {
+    autoSearchPaths = true,
+    useLibraryCodeForTypes = true,
+    diagnosticMode = 'openFilesOnly',
+  },
+}
+
+---@param client vim.lsp.Client
+---@param path string
+local function pyright_set_python_path(client, path)
+  if client.settings then
+    client.settings.python = vim.tbl_deep_extend(
+      'force',
+      client.settings.python,
+      { pythonPath = path }
+    )
+  else
+    client.config.settings = vim.tbl_deep_extend(
+      'force',
+      client.config.settings,
+      { python = { pythonPath = path } }
+    )
+  end
+  client.notify('workspace/didChangeConfiguration', { settings = nil })
+end
+
+---@param client vim.lsp.Client
+---@param name string
+local function pyright_organize_imports(client, name)
+  client.request('workspace/executeCommand', {
+    command = string.format('%s.organizeimports', name),
+    arguments = { vim.uri_from_bufnr(0) },
+  }, nil, 0)
+end
+
+---@param name string
+---@return fun(vim.lsp.Client)
+local function pyright_on_attach(name)
+  return function(client)
+    if disable_formatting then
+      disable_formatting(client)
+    end
+    vim.api.nvim_buf_create_user_command(
+      0,
+      'PyrightOrganizeImports',
+      function()
+        pyright_organize_imports(client, name)
+      end,
+      { desc = 'Organize python imports' }
+    )
+    vim.api.nvim_buf_create_user_command(
+      0,
+      'PyrightSetPythonPath',
+      function(args)
+        pyright_set_python_path(client, args.args)
+      end,
+      {
+        desc = string.format(
+          'Reconfigure %s with the provided python path',
+          name
+        ),
+        nargs = 1,
+        complete = 'file',
+      }
+    )
+  end
+end
+
 local server_configs = {
   {
-    cmd = { 'pyright-langserver', '--stdio' },
-    root_patterns = vim.list_extend({ 'pyrightconfig.json' }, root_patterns),
-    on_attach = function(client)
-      if disable_formatting then
-        disable_formatting(client)
-      end
-      vim.api.nvim_buf_create_user_command(
-        0,
-        'PyrightOrganizeImports',
-        function()
-          client.request('workspace/executeCommand', {
-            command = 'pyright.organizeimports',
-            arguments = { vim.uri_from_bufnr(0) },
-          }, nil, 0)
-        end,
-        { desc = 'Organize python imports' }
-      )
-      vim.api.nvim_buf_create_user_command(
-        0,
-        'PyrightSetPythonPath',
-        function(args)
-          if client.settings then
-            client.settings.python = vim.tbl_deep_extend(
-              'force',
-              client.settings.python,
-              { pythonPath = args.args }
-            )
-          else
-            client.config.settings = vim.tbl_deep_extend(
-              'force',
-              client.config.settings,
-              { python = { pythonPath = args.args } }
-            )
-          end
-          client.notify('workspace/didChangeConfiguration', { settings = nil })
-        end,
-        {
-          desc = 'Reconfigure pyright with the provided python path',
-          nargs = 1,
-          complete = 'file',
-        }
-      )
-    end,
+    cmd = { 'basedpyright-langserver', '--stdio' },
+    root_patterns = pyright_root_patterns,
+    on_attach = pyright_on_attach('basedpyright'),
     settings = {
-      python = {
-        analysis = {
-          autoSearchPaths = true,
-          useLibraryCodeForTypes = true,
-          diagnosticMode = 'openFilesOnly',
-        },
-      },
+      basedpyright = pyright_settings,
+    },
+  },
+  {
+    cmd = { 'pyright-langserver', '--stdio' },
+    root_patterns = pyright_root_patterns,
+    on_attach = pyright_on_attach('pyright'),
+    settings = {
+      python = pyright_settings,
     },
   },
   {
