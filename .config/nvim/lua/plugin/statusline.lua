47092ebed35d2cb53f8c99d67ba8053d74e917cc
bekaboo <kankefengjing@gmail.com>
2025-02-06 11:55:31 -0500

    refactor(nvim): extract statusline spinner to `utils.stl`

--- .config/nvim/lua/plugin/statusline.lua
+++ .config/nvim/lua/plugin/statusline.lua
@@ -432,135 +432,70 @@ function _G._statusline.diag()
   return str
 end
 
-local spinner_end_keep = 2000 -- ms
-local spinner_status_keep = 600 -- ms
-local spinner_progress_keep = 80 -- ms
-local spinner_timer = vim.uv.new_timer()
-
-local spinner_icons ---@type string[]
-local spinner_icon_done ---@type string
-
-if vim.g.has_nf then
-  spinner_icon_done = vim.trim(utils.static.icons.Ok)
-  spinner_icons = {
-    '⣷',
-    '⣯',
-    '⣟',
-    '⡿',
-    '⢿',
-    '⣻',
-    '⣽',
-    '⣾',
-  }
-else
-  spinner_icon_done = '[done]'
-  spinner_icons = {
-    '[    ]',
-    '[=   ]',
-    '[==  ]',
-    '[=== ]',
-    '[ ===]',
-    '[  ==]',
-    '[   =]',
-  }
-end
+---@type stl_spinner_t
+local spinner = utils.stl.spinner:new()
 
 ---Id and additional info of language servers in progress
----@type table<integer, { name: string, timestamp: integer, type: 'begin'|'report'|'end' }>
-local server_info = {}
+---@type table<integer, { name: string, bufs: integer[] }>
+local client_info = {}
+
+vim.api.nvim_create_autocmd('LspDetach', {
+  desc = 'Clean up server info when client detaches.',
+  group = groupid,
+  callback = function(info)
+    if info.data.client_id then
+      client_info[info.data.client_id] = nil
+    end
+  end,
+})
 
 vim.api.nvim_create_autocmd('LspProgress', {
   desc = 'Update LSP progress info for the status line.',
   group = groupid,
   callback = function(info)
-    if spinner_timer then
-      spinner_timer:start(
-        spinner_progress_keep,
-        spinner_progress_keep,
-        vim.schedule_wrap(vim.cmd.redrawstatus)
-      )
+    -- Update LSP progress data
+    local id = info.data.client_id
+    client_info[id] = {
+      name = vim.lsp.get_client_by_id(id).name,
+      bufs = vim.lsp.get_buffers_by_client_id(id),
+    }
+
+    if spinner.status == 'idle' then
+      spinner:spin()
     end
 
-    local id = info.data.client_id
-    local now = vim.uv.now()
-    server_info[id] = {
-      name = vim.lsp.get_client_by_id(id).name,
-      timestamp = now,
-      type = info.data
-        and info.data.params
-        and info.data.params.value
-        and info.data.params.value.kind,
-    } -- Update LSP progress data
-    -- Clear client message after a short time if no new message is received
-    vim.defer_fn(function()
-      -- No new report since the timer was set
-      local last_timestamp = (server_info[id] or {}).timestamp
-      if not last_timestamp or last_timestamp == now then
-        server_info[id] = nil
-        if vim.tbl_isempty(server_info) and spinner_timer then
-          spinner_timer:stop()
-        end
-        vim.cmd.redrawstatus()
-      end
-    end, spinner_end_keep)
-
-    vim.cmd.redrawstatus({
-      mods = { emsg_silent = true },
-    })
+    local type = info.data
+      and info.data.params
+      and info.data.params.value
+      and info.data.params.value.kind
+    if type == 'end' then
+      spinner:finish()
+    end
   end,
 })
 
 ---@return string
 function _G._statusline.lsp_progress()
-  if vim.tbl_isempty(server_info) then
+  if spinner.icon == '' then
     return ''
   end
 
   local buf = vim.api.nvim_get_current_buf()
-  local server_ids = {}
-  for id, _ in pairs(server_info) do
-    if vim.tbl_contains(vim.lsp.get_buffers_by_client_id(id), buf) then
-      table.insert(server_ids, id)
-    end
-  end
-  if vim.tbl_isempty(server_ids) then
+  local buf_clients = vim.iter(vim.tbl_keys(client_info)):filter(function(id)
+    return vim.tbl_contains(client_info[id].bufs, buf)
+  end)
+
+  if not buf_clients:peek() then
     return ''
   end
 
-  local now = vim.uv.now()
-  ---@return boolean
-  local function allow_changing_state()
-    return not vim.b.spinner_state_changed
-      or now - vim.b.spinner_state_changed > spinner_status_keep
-  end
+  local client_names = buf_clients
+    :map(function(id)
+      return client_info[id].name
+    end)
+    :join(', ')
 
-  if #server_ids == 1 and server_info[server_ids[1]].type == 'end' then
-    if vim.b.spinner_icon ~= spinner_icon_done and allow_changing_state() then
-      vim.b.spinner_state_changed = now
-      vim.b.spinner_icon = spinner_icon_done
-    end
-  else
-    local spinner_icon_progress = spinner_icons[math.ceil(
-      now / spinner_progress_keep
-    ) % #spinner_icons + 1]
-    if vim.b.spinner_icon ~= spinner_icon_done then
-      vim.b.spinner_icon = spinner_icon_progress
-    elseif allow_changing_state() then
-      vim.b.spinner_state_changed = now
-      vim.b.spinner_icon = spinner_icon_progress
-    end
-  end
-
-  return string.format(
-    '%s %s ',
-    table.concat(
-      vim.tbl_map(function(id)
-        return utils.stl.escape(server_info[id].name)
-      end, server_ids),
-      ', '
-    ),
-    vim.b.spinner_icon
-  )
+  return string.format('%s %s ', client_names, spinner.icon)
 end
 
 -- stylua: ignore start
--- .config/nvim/lua/utils/stl.lua
+++ .config/nvim/lua/utils/stl.lua
@@ -30,4 +30,150 @@ function M.escape(str)
   return (str:gsub('%%', '%%%%'))
 end
 
+---@class stl_spinner_t
+---@field opts stl_spinner_opts_t
+---@field timer uv.uv_timer_t
+---@field icon string icon of the spinner
+---@field changed_tick integer timestamp when the spinner icons is changed last time
+---@field status 'spinning'|'finish'|'idle'
+M.spinner = {}
+
+---@class stl_spinner_opts_t
+---@field frame_interval? integer time interval between spinner frames (ms)
+---@field finish_timeout? integer time to show finish icon before clearing (ms)
+---@field icons? { progress: string[], finish: string }
+---@field on_spin? fun(spinner: stl_spinner_t) function to execute on each update
+---@field on_finish? fun(spinner: stl_spinner_t) function to execute on stop
+
+---@type stl_spinner_opts_t
+M.spinner.default_opts = {
+  frame_interval = 80,
+  finish_timeout = 1000,
+  icons = {
+    progress = vim.g.has_nf
+        and { '⣷', '⣯', '⣟', '⡿', '⢿', '⣻', '⣽', '⣾' }
+      or {
+        '[    ]',
+        '[=   ]',
+        '[==  ]',
+        '[=== ]',
+        '[ ===]',
+        '[  ==]',
+        '[   =]',
+      },
+    finish = vim.g.has_nf and vim.trim(require('utils.static.icons').Ok)
+      or '[done]',
+  },
+}
+
+---Create a new spinner instance
+---@param opts stl_spinner_opts_t?
+---@return stl_spinner_t
+function M.spinner:new(opts)
+  return setmetatable({
+    opts = vim.tbl_deep_extend('keep', opts or {}, M.spinner.default_opts),
+    timer = vim.uv.new_timer(),
+    icon = '',
+    last_change = vim.uv.now(),
+    status = 'idle',
+  }, { __index = self })
+end
+
+---Delete the spinner instance and clean up resources
+function M.spinner:del()
+  if self.status == 'spinning' then
+    self:finish()
+  end
+  self.timer:close()
+  for key, _ in pairs(self) do
+    self[key] = nil
+  end
+end
+
+---Start or continue spinning animation
+---@param on_spin? fun(spinner: stl_spinner_t)
+function M.spinner:spin(on_spin)
+  on_spin = on_spin or self.opts.on_spin
+
+  local now = vim.uv.now()
+
+  -- Don't interrupt finish state if it hasn't displayed for
+  -- `finish_timeout` ms
+  if
+    self.status == 'finish'
+    and now - self.changed_tick < self.opts.finish_timeout
+  then
+    return
+  end
+
+  self.changed_tick = now
+  self.icon = self.opts.icons.progress[math.ceil(
+    now / self.opts.frame_interval
+  ) % #self.opts.icons.progress + 1]
+
+  -- Start timer if not already spinning
+  if self.status ~= 'spinning' then
+    self.status = 'spinning'
+    self.timer:start(
+      0,
+      self.opts.frame_interval,
+      vim.schedule_wrap(function()
+        self:spin(on_spin)
+      end)
+    )
+  end
+
+  if on_spin then
+    on_spin(self)
+  end
+
+  self:redraw()
+end
+
+---Show finish icon and stop spinning
+---@param on_finish? fun(spinner: stl_spinner_t)
+function M.spinner:finish(on_finish)
+  on_finish = on_finish or self.opts.on_finish
+
+  local now = vim.uv.now()
+  if self.icon ~= self.opts.icons.finish then
+    self.changed_tick = now
+    self.icon = self.opts.icons.finish
+  end
+
+  -- Stop timer if spinning
+  if self.status == 'spinning' then
+    self.status = 'finish'
+    self.timer:stop()
+    if on_finish then
+      on_finish(self)
+    end
+  end
+
+  self:redraw()
+
+  -- Clear the icon after timeout
+  vim.defer_fn(function()
+    local n = vim.uv.now()
+    -- Don't clear if not in `finish` state or icon changed after this
+    -- `spinner:finish()` call
+    if self.status ~= 'finish' or self.changed_tick ~= now then
+      return
+    end
+    self.changed_tick = n
+    self.icon = ''
+    self.status = 'idle'
+    self:redraw()
+  end, self.opts.finish_timeout)
+end
+
+---Redraw so that the new icon can be shown in statusline
+function M.spinner.redraw()
+  vim.cmd.redrawstatus({
+    mods = {
+      emsg_silent = true,
+    },
+  })
+end
+
 return M
