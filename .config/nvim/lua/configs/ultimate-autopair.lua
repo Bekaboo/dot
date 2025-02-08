195ec8df98d12c33d12f93996e12e47c497ad7e0
bekaboo <kankefengjing@gmail.com>
2025-02-07 22:38:15 -0500

    chore(nvim): update plugins

--- .config/nvim/lua/configs/ultimate-autopair.lua
+++ .config/nvim/lua/configs/ultimate-autopair.lua
@@ -1,5 +1,4 @@
 local ap_utils = require('ultimate-autopair.utils')
-local ap_core = require('ultimate-autopair.core')
 
 ---Filetype options memoization
 ---@type table<string, table<string, string|integer|boolean|table>>
@@ -30,30 +29,6 @@ ap_utils.getsmartft = (function(cb)
     return cb(o, vim.b.bigfile or notree, ...)
   end
 end)(ap_utils.getsmartft)
-
--- Set 'lazyredraw' on paring to prevent cursor jump caused by `<C-g>U<Left>`
--- Source: https://github.com/windwp/nvim-autopairs/pull/403
--- TODO: upstream
-ap_core.run_run = (function(cb)
-  local lz
-
-  return function(...)
-    if not vim.go.lz then
-      lz = vim.go.lz
-      vim.go.lz = true
-
-      vim.schedule(function()
-        if lz == nil then
-          return
-        end
-        vim.go.lz = lz
-        lz = nil
-      end)
-    end
-
-    return cb(...)
-  end
-end)(ap_core.run_run)
 
 ---Get next two characters after cursor
 ---@return string: next two characters
