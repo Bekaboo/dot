153de97df8d0c7af15bd5ee98f58c9f26473c121
bekaboo <kankefengjing@gmail.com>
2025-02-08 02:37:55 -0500

    refactor(fish): clean up theme reload logic

--- .config/fish/conf.d/theme.fish
+++ .config/fish/conf.d/theme.fish
@@ -2,14 +2,13 @@ if not status is-interactive
     exit
 end
 
-function __fish_reload_theme \
-    --on-variable __fish_reload_theme \
+function __fish_reload_theme --on-variable __fish_reload_theme \
     --description 'Switch fish theme'
-    type -q tput
-    and test (tput colors 2>/dev/null) -ge 256
-    and test -f "$__fish_config_dir/themes/Current.theme"
-    and fish_config theme choose Current 2>/dev/null
-    or fish_config theme choose 'Default Dark' 2>/dev/null
+    if type -q tput; and test (tput colors 2>/dev/null) -lt 256
+        fish_config theme choose 'Default Dark' 2>/dev/null
+        return
+    end
+    fish_config theme choose Current 2>/dev/null
     commandline -f repaint
 end
 
