f7b4b5e034694f7fe50d18d2de8238db1acf061f
bekaboo <kankefengjing@gmail.com>
2025-02-11 13:22:46 -0500

    fix(nvim): molten keymaps not loaded on 2nd jupyter notebook

--- .config/nvim/lua/modules/markup.lua
+++ .config/nvim/lua/modules/markup.lua
@@ -43,20 +43,62 @@ return {
   {
     'benlubas/molten-nvim',
     init = function()
+      local loaded = false
+
       ---Lazy-load molten plugin on keys
       ---@param mode string in which mode to set the triggering keymap
       ---@param key string the key to trigger molten plugin
       ---@param buf number the buffer to set the keymap
       ---@param opts vim.keymap.set.Opts? the keymap options
-      local function load(mode, key, buf, opts)
+      local function load_on_key(mode, key, buf, opts)
+        if loaded then
+          return
+        end
+
+        if not opts then
+          opts = { buffer = buf }
+        else
+          opts.buffer = buf
+        end
+
         vim.keymap.set(mode, key, function()
           require('molten.status')
+          loaded = true
           vim.api.nvim_feedkeys(
             vim.api.nvim_replace_termcodes(key, true, true, true),
             'im',
             false
           )
-        end, vim.tbl_deep_extend('force', { buffer = buf }, opts or {}))
+        end, opts)
+      end
+
+      ---@param buf integer
+      local function set_triggers(buf)
+        if not vim.api.nvim_buf_is_valid(buf) then
+          return
+        end
+
+        local ft = vim.bo[buf].ft
+        if
+          ft ~= 'python' and ft ~= 'markdown'
+          or ft == 'markdown' -- don't load in normal markdown files
+            and vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf), ':e') ~= 'ipynb'
+        then
+          return
+        end
+
+        -- For both python and notebook buffers
+        load_on_key('x', '<CR>', buf, { desc = 'Run current cell' })
+
+        -- Jupyter notebook only keymaps
+        if vim.bo[buf].ft == 'markdown' then
+          -- stylua: ignore start
+          load_on_key('n', '<CR>', buf, { desc = 'Run current cell' })
+          load_on_key('n', '<LocalLeader>k', buf, { desc = 'Run current cell and all above' })
+          load_on_key('n', '<LocalLeader>j', buf, { desc = 'Run current cell and all below' })
+          load_on_key('n', '<LocalLeader><CR>', buf, { desc = 'Run code selected by operator' })
+          -- stylua: ignore end
+        end
       end
 
       vim.api.nvim_create_autocmd('FileType', {
@@ -64,24 +106,10 @@ return {
         group = vim.api.nvim_create_augroup('MoltenLazyLoadKeys', {}),
         pattern = { 'python', 'markdown' },
         callback = function(info)
-          -- Skip for non-notebook markdown files
-          if
-            info.match == 'markdown'
-            and vim.fn.fnamemodify(info.file, ':e') ~= 'ipynb'
-          then
-            return
+          if loaded then
+            return true
           end
-
-          load('x', '<CR>', info.buf, { desc = 'Run current cell' }) -- for both python and notebook buffers
-          if info.match == 'markdown' then
-            -- stylua: ignore start
-            load('n', '<CR>', info.buf, { desc = 'Run current cell' })
-            load('n', '<LocalLeader>k', info.buf, { desc = 'Run current cell and all above' })
-            load('n', '<LocalLeader>j', info.buf, { desc = 'Run current cell and all below' })
-            load('n', '<LocalLeader><CR>', info.buf, { desc = 'Run code selected by operator' })
-            -- stylua: ignore end
-          end
-          return true
+          set_triggers(info.buf)
         end,
       })
     end,
