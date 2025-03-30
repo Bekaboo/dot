bba98df2b9b1c3f2d5214ce7ed2e3cf956a7d050
bekaboo <kankefengjing@gmail.com>
2025-03-30 12:29:34 -0400

    fix(nvim): nvim-treesitter-endwise not working

--- .config/nvim/lua/configs/nvim-treesitter-endwise.lua
+++
@@ -1,6 +0,0 @@
----@diagnostic disable-next-line: missing-fields
-require('nvim-treesitter.configs').setup({
-  endwise = {
-    enable = true,
-  },
-})
--- .config/nvim/lua/modules/treesitter.lua
+++ .config/nvim/lua/modules/treesitter.lua
@@ -46,7 +46,9 @@ return {
     dependencies = 'nvim-treesitter/nvim-treesitter',
     event = 'InsertEnter',
     config = function()
-      require('configs.nvim-treesitter-endwise')
+      -- Manually trigger `FileType` event to make nvim-treesitter-endwise
+      -- attach to current file when loaded
+      vim.api.nvim_exec_autocmds('FileType', {})
     end,
   },
 
