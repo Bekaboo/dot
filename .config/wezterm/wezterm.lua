356d9749f4b94798c834ffa8f1a86d609735d7e3
bekaboo <kankefengjing@gmail.com>
2025-05-14 08:42:38 -0700

    fix(alacritty,kitty,wezterm): use option as alt key on macOS

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,20 +2,26 @@ local wezterm = require('wezterm')
 local config = wezterm.config_builder and wezterm.config_builder() or {}
 local config_dir = wezterm.config_dir
 
+config.term = 'wezterm'
 config.automatically_reload_config = true
 config.animation_fps = 1 -- Disable cursor blinking easing animation
 config.color_scheme_dirs = { config_dir .. '/colors' }
 config.color_scheme = 'Dragon Dark' -- Default colorscheme
 config.check_for_updates = false
 config.enable_tab_bar = false
+
+-- Use both left+right option key as alt on macOS
+config.send_composed_key_when_left_alt_is_pressed = false
+config.send_composed_key_when_right_alt_is_pressed = false
+
+config.font_size = 12
 config.font = wezterm.font_with_fallback({
   { family = 'Iosevka NerdFont' },
   { family = 'Microsoft YaHei' },
 })
-config.font_size = 12
+
 config.initial_rows = 24
 config.initial_cols = 112
-config.term = 'wezterm'
 config.window_padding = {
   left = '1cell',
   right = '1cell',
