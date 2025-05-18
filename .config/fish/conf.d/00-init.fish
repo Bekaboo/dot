fbde9b05d8af8d07ec7db8823e1604fc725af27d
bekaboo <kankefengjing@gmail.com>
2025-05-18 16:20:08 -0700

    refactor(fish): clean up

--- .config/fish/conf.d/00-init.fish
+++ .config/fish/conf.d/00-init.fish
@@ -1,12 +1,9 @@
 # Initialize fish shell, including settings global variables etc.
 
 # Setup for macOS homebrew
-if test (uname) = Darwin
-    fish_add_path -p /opt/homebrew/bin /usr/local/bin
-
-    if type -q brew
-        eval (brew shellenv)
-    end
+if type -q brew
+    fish_add_path -p (brew --prefix)/bin
+    eval (brew shellenv)
 end
 
 # Other install paths
@@ -24,23 +21,21 @@ if test -f $__fish_config_dir/fish_envvars
     source $__fish_config_dir/fish_envvars
 end
 
-if type -q nvim
-    set -gx EDITOR nvim
-    set -gx MANPAGER 'nvim +Man!'
-else if type -q vim
-    set -gx EDITOR vim
-else if type -q vi
-    set -gx EDITOR vi
-end
-
-if type -q xhost
-    xhost +local:root &>/dev/null
+for editor in nvim vim vi
+    if type -q $editor
+        set -gx EDITOR $editor
+        if test $editor = nvim
+            set -gx MANPAGER 'nvim +Man!'
+        end
+        break
+    end
 end
 
 # Set rg config path
 set -gx RIPGREP_CONFIG_PATH $HOME/.ripgreprc
 
-if type -q proot-distro
+if status is-login
+    and type -q proot-distro
     and test -n "$TERMUX_VERSION"
     and test -n "$PROOT_DISTRO"
     and test -n "$PROOT_USER"
