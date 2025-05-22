6fe3b8fbcc561517e35cb2ca7c9e012a19b5d016
bekaboo <kankefengjing@gmail.com>
2025-05-22 00:44:20 -0700

    feat(nvim): add lua module test projectionist heuristic rules

--- .config/nvim/lua/projections/lua.lua
+++ .config/nvim/lua/projections/lua.lua
@@ -7,7 +7,9 @@ return {
     ['lua/*.lua'] = {
       alternate = {
         'tests/{}_spec.lua',
+        'tests/{dirname}_spec.lua', -- module test
         'tests/{dirname|tail}/{basename}_spec.lua',
+        'tests/{dirname|tail}_spec.lua', -- module test
       },
       type = 'source',
     },
@@ -16,7 +18,9 @@ return {
         -- Guess lua module name from project directory name,
         -- not always correct
         'lua/{project|basename|root}/{}.lua',
+        'lua/{project|basename|root}/{}/init.lua', -- module test
         'lua/{}.lua',
+        'lua/{}/init.lua', -- module test
       },
       type = 'test',
     },
