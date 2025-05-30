bd8cc3860438b87305820ea30e998f8163833fc6
bekaboo <kankefengjing@gmail.com>
2025-05-30 08:07:51 -0700

    fix(fish): don't error in completion if `hidutil` not available

--- .config/fish/completions/macos-keymap.fish
+++ .config/fish/completions/macos-keymap.fish
@@ -4,5 +4,5 @@ complete -c macos-keymap -s h -l help -d "Show help message"
 complete -c macos-keymap -s r -l reset -d "Reset keyboard mapping to default"
 
 # Vendor/Product ID arguments
-complete -c macos-keymap -n "not __fish_contains_opt h help r reset" -d "Vendor ID" -a "(hidutil list | awk '/Keyboard/{print \$1}')"
-complete -c macos-keymap -n "not __fish_contains_opt h help r reset" -d "Product ID" -a "(hidutil list | awk '/Keyboard/{print \$2}')"
+complete -c macos-keymap -n "not __fish_contains_opt h help r reset" -d "Vendor ID" -a "(type -q hidutil; and hidutil list | awk '/Keyboard/{print \$1}')"
+complete -c macos-keymap -n "not __fish_contains_opt h help r reset" -d "Product ID" -a "(type -q hidutil; and hidutil list | awk '/Keyboard/{print \$2}')"
