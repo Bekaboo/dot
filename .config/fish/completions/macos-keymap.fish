a7b485111a22cc608cfeb08cc3422670bce4669e
bekaboo <kankefengjing@gmail.com>
2025-05-30 07:47:36 -0700

    feat(macos,bin,fish): automate key remapping on macOS

---
+++ .config/fish/completions/macos-keymap.fish
@@ -0,0 +1,8 @@
+complete -f -c macos-keymap
+
+complete -c macos-keymap -s h -l help -d "Show help message"
+complete -c macos-keymap -s r -l reset -d "Reset keyboard mapping to default"
+
+# Vendor/Product ID arguments
+complete -c macos-keymap -n "not __fish_contains_opt h help r reset" -d "Vendor ID" -a "(hidutil list | awk '/Keyboard/{print \$1}')"
+complete -c macos-keymap -n "not __fish_contains_opt h help r reset" -d "Product ID" -a "(hidutil list | awk '/Keyboard/{print \$2}')"
