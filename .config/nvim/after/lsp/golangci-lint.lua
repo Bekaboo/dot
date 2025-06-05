c245d6e0ac36c148f001c85d7cacb6eb91360e39
bekaboo <kankefengjing@gmail.com>
2025-06-05 09:04:14 -0700

    fix(nvim): adapt to golangci-lint v2 breaking change

--- .config/nvim/after/lsp/golangci-lint.lua
+++ .config/nvim/after/lsp/golangci-lint.lua
@@ -19,7 +19,9 @@ return {
         {
           -- Must run on the package dir to lint all files, else the linter
           -- will fail to find definitions in other files in the same package
-          lintCommand = 'golangci-lint run --color never --out-format tab --enable exhaustruct "$(dirname "${INPUT}")"',
+          -- `--output-format` is removed in golangci-lint v2, see:
+          -- https://github.com/golangci/golangci-lint/discussions/5612#discussioncomment-12607774
+          lintCommand = 'golangci-lint run --color never --output.tab.path stdout --enable exhaustruct "$(dirname "${INPUT}")"',
           lintFormats = { '%f:%l:%c%*\\s%*\\S%*\\s%m' },
           lintSource = 'golangci-lint',
           lintStdin = false,
