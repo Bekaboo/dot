2edaa8db22eff55e4bd8922020aba17707eb2c3f
bekaboo <kankefengjing@gmail.com>
2025-06-22 21:42:09 -0700

    fix(fish): error completing `bak` command

--- .config/fish/completions/bak.fish
+++ .config/fish/completions/bak.fish
@@ -2,10 +2,10 @@
 complete -c bak -f
 
 # Main options
-complete -c bak -s h -l help -d "Show help message"
-complete -c bak -s m -l move -d "Use move instead of copy"
-complete -c bak -s R -l restore -d "Restore from backup"
+complete -c bak -s h -l help -d 'Show help message'
+complete -c bak -s m -l move -d 'Use move instead of copy'
+complete -c bak -s R -l restore -d 'Restore from backup'
 
 # Condition-sensitive argument completion
-complete -c bak -n "__fish_contains_opt -s R restore" -a "(__fish_suffix .bak)"
-complete -c bak -n "not __fish_contains_opt -s R restore" -a "(__fish_complete_path)"
+complete -c bak -n '__fish_contains_opt -s R restore' -a '(find . -maxdepth 1 -name "*.bak" -type f 2>/dev/null | sed "s|^\./||")'
+complete -c bak -n 'not __fish_contains_opt -s R restore' -a '(__fish_complete_path)'
