58254f92e50fbcc50f89406198e1b43eaf026d9c
bekaboo <kankefengjing@gmail.com>
2025-06-30 13:23:59 -0700

    refactor(nvim): disable diagnostics from protols (too many false alarms)

--- .config/nvim/after/lsp/protols.lua
+++ .config/nvim/after/lsp/protols.lua
@@ -2,4 +2,9 @@ return {
   filetypes = { 'proto' },
   cmd = { 'protols' },
   root_markers = { 'protols.toml' },
+  handlers = {
+    -- Disable diagnostics from protols as it has too many false alarms
+    -- Method to disable diagnostics comes from https://www.reddit.com/r/neovim/comments/13qurat/how_to_disable_diagnostics_for_specific_lsp_server
+    ['textDocument/publishDiagnostics'] = function() end,
+  },
 }
