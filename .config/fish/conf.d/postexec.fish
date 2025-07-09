56d892d8a94700f403c46b658614099a19220c04
bekaboo <kankefengjing@gmail.com>
2025-07-09 11:18:44 -0700

    fix(fish): error running postexec if `$cmd` starts with '-'

--- .config/fish/conf.d/postexec.fish
+++ .config/fish/conf.d/postexec.fish
@@ -10,7 +10,7 @@ function __postexec_append_newline --on-event fish_postexec \
     # Don't add extra newline if the commandline is 'clear',
     # The commandline is passed as the first parameter,
     # see https://fishshell.com/docs/current/language.html#event
-    if string match -aqr $cmd '^\\s*clear\\s*$'
+    if string match -aqr -- $cmd '^\\s*clear\\s*$'
         return
     end
     echo
