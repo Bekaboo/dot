ab781dc4ce31d12cf3ecf5a73cba9d5af3eb7e24
bekaboo <kankefengjing@gmail.com>
2025-07-15 09:29:49 -0700

    refactor(fish): remove hack for `clear`

--- .config/fish/conf.d/postexec.fish
+++ .config/fish/conf.d/postexec.fish
@@ -7,11 +7,5 @@ end
 function __postexec_append_newline --on-event fish_postexec \
     --description 'Add newline before prompt only when previous output exists' \
     --argument-names cmd
-    # Don't add extra newline if the commandline is 'clear',
-    # The commandline is passed as the first parameter,
-    # see https://fishshell.com/docs/current/language.html#event
-    if string match -aqr -- $cmd '^\\s*clear\\s*$'
-        return
-    end
     echo
 end
