c89111515f388766122e46ed205d918a6a2d4b1b
bekaboo <kankefengjing@gmail.com>
2025-07-16 16:53:56 -0700

    feat(nvim): export backet paste start/end sequences in `utils.term`

--- .config/nvim/lua/utils/term.lua
+++ .config/nvim/lua/utils/term.lua
@@ -117,8 +117,8 @@ function M.compose_name(bufname, opts)
   )
 end
 
-local BRACKET_PASTE_START = '\27[200~'
-local BRACKET_PASTE_END = '\27[201~'
+M.BRACKET_PASTE_START = '\27[200~'
+M.BRACKET_PASTE_END = '\27[201~'
 
 ---Send multi-line message to terminal
 ---@param msg string|string[] message
@@ -143,7 +143,7 @@ function M.send(msg, buf)
 
   vim.api.nvim_chan_send(
     chan,
-    BRACKET_PASTE_START .. table.concat(msg, '\n') .. BRACKET_PASTE_END
+    M.BRACKET_PASTE_START .. table.concat(msg, '\n') .. M.BRACKET_PASTE_END
   )
 end
 
