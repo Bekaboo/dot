bfe83c2e205060bba3496f77241e766619015678
bekaboo <kankefengjing@gmail.com>
2025-07-21 11:35:59 -0700

    feat(nvim): use the same indent settings in shfmt as in nvim

--- .config/nvim/after/lsp/shfmt.lua
+++ .config/nvim/after/lsp/shfmt.lua
@@ -8,7 +8,10 @@ return {
     languages = {
       sh = {
         {
-          formatCommand = 'shfmt --filename ${INPUT} -',
+          -- If `expandtab` is not set, use tabs for indentation, else use
+          -- spaces with the same width in nvim, see `shfmt -h` and
+          -- https://github.com/mattn/efm-langserver/blob/master/schema.md#languages_pattern1_items_format-can-range
+          formatCommand = 'shfmt --filename ${INPUT} --indent=${tabWidth} ${--indent=0:!insertSpaces} -',
           formatStdin = true,
         },
       },
