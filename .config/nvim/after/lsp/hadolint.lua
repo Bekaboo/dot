e98d7660111d4e789f97e33be93c4593419698a1
bekaboo <kankefengjing@gmail.com>
2025-07-28 18:07:31 -0700

    feat(nvim): add hadolint lsp config

---
+++ .config/nvim/after/lsp/hadolint.lua
@@ -0,0 +1,27 @@
+-- Dockerfile linter, validate inline bash, written in Haskell
+-- https://github.com/hadolint/hadolint
+
+---@type lsp_config_t
+return {
+  filetypes = { 'dockerfile' },
+  cmd = { 'efm-langserver' },
+  requires = { 'hadolint' },
+  name = 'hadolint',
+  root_markers = { '.hadolint.yaml' },
+  settings = {
+    languages = {
+      dockerfile = {
+        {
+          lintSource = 'hadolint',
+          lintCommand = 'hadolint --format gnu ${INPUT}',
+          lintFormats = {
+            'hadolint:%f:%l: SC%n %t%\\w\\+: %m',
+          },
+          lintStdin = false,
+          lintOffset = -1,
+          lintAfterOpen = true,
+        }
+      },
+    },
+  },
+}
