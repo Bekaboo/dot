8b3804dd48035fd10729b870484135d6a825a09c
bekaboo <kankefengjing@gmail.com>
2025-07-29 21:53:45 -0700

    fix(nvim): luacheck ignores per-file rules in `.luacheckrc`

--- .config/nvim/after/lsp/luacheck.lua
+++ .config/nvim/after/lsp/luacheck.lua
@@ -10,7 +10,13 @@ return {
       lua = {
         {
           lintSource = 'luacheck',
-          lintCommand = 'luacheck --codes --no-color --quiet -',
+          -- Use `--filename` tell luacheck the source file of stdin and apply
+          -- per-files rules defined in `.luacheckrc` and per-path std
+          -- overrides:
+          -- - https://luacheck.readthedocs.io/en/stable/config.html#per-file-and-per-path-overrides
+          -- - https://luacheck.readthedocs.io/en/stable/config.html#default-per-path-std-overrides
+          -- - https://luacheck.readthedocs.io/en/stable/cli.html#stable-interface-for-editor-plugins-and-tools
+          lintCommand = 'luacheck --codes --no-color --quiet --filename ${INPUT} -',
           lintFormats = { '%.%#:%l:%c: (%t%n) %m' },
           lintAfterOpen = true,
           lintStdin = true,
