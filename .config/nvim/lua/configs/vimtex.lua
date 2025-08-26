0ccc43e1e72d470f88a3fef562c729d1bd12af38
bekaboo <kankefengjing@gmail.com>
2025-08-25 21:40:22 -0400

    fix(nvim): vimtex warning about highlighting taken by treesitter in tex files

--- .config/nvim/lua/configs/vimtex.lua
+++ .config/nvim/lua/configs/vimtex.lua
@@ -2,6 +2,13 @@ if vim.env.TERM == 'linux' then
   vim.g.vimtex_syntax_conceal_disable = true
 end
 
+-- Disable vimtex warning about highlighting if we have latex treesitter parser
+-- as we can use treesitter instead of vimtex for latex mathzone detection, see
+-- https://github.com/lervag/vimtex/issues/2469#issuecomment-1446685300
+if pcall(vim.treesitter.get_parser, nil, 'latex') then
+  vim.g.vimtex_syntax_enabled = 0
+end
+
 vim.g.vimtex_quickfix_mode = 0
 vim.g.vimtex_format_enabled = 1
 vim.g.vimtex_imaps_enabled = 0
