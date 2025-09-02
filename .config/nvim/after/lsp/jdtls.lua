bd4b9759473838c2b377d52a00baf54d4e430a07
bekaboo <kankefengjing@gmail.com>
2025-09-01 22:20:54 -0400

    feat(nvim): add jdtls lsp config

---
+++ .config/nvim/after/lsp/jdtls.lua
@@ -0,0 +1,43 @@
+-- Java language server
+-- https://github.com/eclipse-jdtls/eclipse.jdt.ls
+
+local cache_home = vim.env.XDG_CACHE_HOME or vim.fs.normalize('~/.cache')
+local jdtls_cache_dir = vim.fs.joinpath(cache_home, 'jdtls')
+local jdtls_config_dir = vim.fs.joinpath(jdtls_cache_dir, 'config')
+local jdtls_workspace_dir = vim.fs.joinpath(jdtls_cache_dir, 'workspace')
+
+-- Config adapted from
+-- https://github.com/neovim/nvim-lspconfig/blob/cb4765526f7201ce4ff0c49888f80c18da614e68/lua/lspconfig/configs/jdtls.lua#L25
+return {
+  cmd = {
+    'jdtls',
+    string.format('-configuration=%s', jdtls_config_dir),
+    string.format('-data=%s', jdtls_workspace_dir),
+    -- JVM args
+    (function()
+      if not vim.env.JDTLS_JVM_ARGS then
+        return
+      end
+      return unpack(vim
+        .iter(require('utils.cmd').split(vim.env.JDTLS_JVM_ARGS))
+        :map(function(arg)
+          return string.format('--jvm-arg=%s', arg)
+        end)
+        :totable())
+    end)(),
+  },
+  filetypes = { 'java' },
+  root_markers = {
+    { 'build.gradle', 'build.gradle.kts' },
+    {
+      'build.xml',
+      'pom.xml',
+      'settings.gradle',
+      'settings.gradle.kts',
+    },
+  },
+  init_options = {
+    workspace = jdtls_workspace_dir,
+    jvm_args = {},
+  },
+}
