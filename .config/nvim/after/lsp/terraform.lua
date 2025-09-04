e77abd72f15098b8d2dedcd4b603151f214b6e53
bekaboo <kankefengjing@gmail.com>
2025-09-04 15:44:25 -0400

    feat(nvim): add lsp config for terraform linter/formatter

---
+++ .config/nvim/after/lsp/terraform.lua
@@ -0,0 +1,35 @@
+-- Format terraform config file with `terraform fmt` and lint with
+-- `terraform validate`
+-- https://developer.hashicorp.com/terraform/cli
+
+return {
+  filetypes = { 'terraform' },
+  cmd = { 'efm-langserver' },
+  requires = { 'terraform' },
+  name = 'terraform',
+  init_options = {
+    documentFormatting = true,
+  },
+  settings = {
+    languages = {
+      terraform = {
+        {
+          formatCommand = 'terraform fmt -',
+          formatStdin = true,
+        },
+        {
+          lintSource = 'terraform',
+          lintCommand = [[terraform validate -json | jq -r '.diagnostics[] | "\(.severity) \(.range.filename):\(.range.start.line):\(.range.start.column): \(.summary). \(.detail)"']],
+          lintFormats = {
+            '%trror %f:%l:%c: %m',
+            '%tarning %f:%l:%c: %m',
+          },
+          lintAfterOpen = true,
+          lintStdin = false,
+          lintWorkSpace = true,
+          lintIgnoreExitCode = true,
+        },
+      },
+    },
+  },
+}
