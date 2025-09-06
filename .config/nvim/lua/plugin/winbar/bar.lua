98e8a3efa37c9f37a145b5c55d1b8a34900a0f72
bekaboo <kankefengjing@gmail.com>
2025-09-06 00:32:21 -0400

    chore(nvim): fix typing in winbar plugin

--- .config/nvim/lua/plugin/winbar/bar.lua
+++ .config/nvim/lua/plugin/winbar/bar.lua
@@ -607,7 +607,8 @@ function winbar_t:pick(idx)
     -- Read the input from user
     local shortcut_read = ''
     for _ = 1, n_chars do
-      shortcut_read = shortcut_read .. vim.fn.nr2char(vim.fn.getchar())
+      shortcut_read = shortcut_read
+        .. vim.fn.nr2char(vim.fn.getchar() --[[@as integer]])
     end
     -- Restore the original content of each component
     for _, component in ipairs(clickables) do
