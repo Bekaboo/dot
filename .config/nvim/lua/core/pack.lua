7622e90405edb0d240cd93a15675c3c66bedf2f4
bekaboo <kankefengjing@gmail.com>
2025-09-15 11:21:16 -0400

    refactor(nvim): simplify `core.pack`

--- .config/nvim/lua/core/pack.lua
+++ .config/nvim/lua/core/pack.lua
@@ -4,6 +4,10 @@ end
 
 local utils = require('utils')
 
+local config_path = vim.fn.stdpath('config') --[[@as string]]
+local specs_start_path = vim.fs.joinpath(config_path, 'lua/pack/specs/start')
+local specs_opt_path = vim.fs.joinpath(config_path, 'lua/pack/specs/opt')
+
 ---@param path string
 ---@return vim.pack.Spec[]
 local function collect_specs(path)
@@ -14,42 +18,31 @@ local function collect_specs(path)
   return specs
 end
 
----Enable plugins
----@param plugins string[]? when omitted, enable all plugins under
----`lua/pack/specs`
-local function enable_plugins(plugins)
-  local config_path = vim.fn.stdpath('config') --[[@as string]]
-
-  if vim.fn.argc(-1) > 0 then
-    utils.pack.add(
-      vim.list_extend(
-        collect_specs(vim.fs.joinpath(config_path, 'lua/pack/specs/start')),
-        collect_specs(vim.fs.joinpath(config_path, 'lua/pack/specs/opt'))
-      )
-    )
-    return
-  end
-
-  -- Defer loading plugin specs if no files are specified
+-- Load and manage all plugin specs on startup upon opening a file
+if vim.fn.argc(-1) > 0 then
   utils.pack.add(
-    collect_specs(vim.fs.joinpath(config_path, 'lua/pack/specs/start'))
-  )
-
-  ---Load plugin specs under `opt` directory
-  local function load_opt()
-    utils.pack.add(
-      collect_specs(vim.fs.joinpath(config_path, 'lua/pack/specs/opt'))
+    vim.list_extend(
+      collect_specs(specs_start_path),
+      collect_specs(specs_opt_path)
     )
-  end
-
-  utils.load.on_events('UIEnter', 'my.pack.load', vim.schedule_wrap(load_opt))
-  utils.load.on_events(
-    { 'CmdUndefined', 'SessionLoadPost', 'FileType' },
-    'my.pack.load',
-    load_opt
   )
+  return
 end
 
--- If launched in vscode, only enable basic plugins
--- TODO: implement plugin param
-enable_plugins(vim.g.vscode and { 'edit', 'treesitter' })
+-- Defer loading plugin specs in `opt` if no files are given
+-- Specs under `start` are always loaded on startup
+utils.pack.add(collect_specs(specs_start_path))
+utils.load.on_events(
+  'UIEnter',
+  'my.pack.load',
+  vim.schedule_wrap(function()
+    utils.pack.add(collect_specs(specs_opt_path))
+  end)
+)
+utils.load.on_events(
+  { 'CmdUndefined', 'SessionLoadPost', 'FileType' },
+  'my.pack.load',
+  function()
+    utils.pack.add(collect_specs(specs_opt_path))
+  end
+)
