05777a88e068d0f27c97ca5157399716d339fa2f
bekaboo <kankefengjing@gmail.com>
2025-10-05 11:42:57 -0400

    revert: "perf(nvim): disable `vim.validate()`"
    This reverts commit 993ea1826e2791ea8c05fa39a3c4efa83f1fbc00.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,10 +6,6 @@
 -- Remove the current directory from `package.path` to avoid errors.
 package.path = package.path:gsub('%./%?%.lua;?', '')
 
--- Disable validation for speed
----@diagnostic disable-next-line: duplicate-set-field
-vim.validate = function() end
-
 -- Enable faster lua loader using byte-compilation
 -- https://github.com/neovim/neovim/commit/2257ade3dc2daab5ee12d27807c0b3bcf103cd29
 vim.loader.enable()
