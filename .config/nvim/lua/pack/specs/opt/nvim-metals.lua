43e67e4ca293fec7455c866d8e2cbdd22251b126
bekaboo <kankefengjing@gmail.com>
2025-10-13 00:45:32 -0400

    feat(nvim): add metals dap config

--- .config/nvim/lua/pack/specs/opt/nvim-metals.lua
+++ .config/nvim/lua/pack/specs/opt/nvim-metals.lua
@@ -2,6 +2,10 @@
 return {
   src = 'https://github.com/scalameta/nvim-metals',
   data = {
+    deps = {
+      src = 'https://github.com/mfussenegger/nvim-dap',
+      data = { optional = true },
+    },
     events = {
       event = 'FileType',
       pattern = { 'scala', 'sbt' },
@@ -11,9 +15,43 @@ return {
         group = vim.api.nvim_create_augroup('my.nvim_metals.init', {}),
         pattern = { 'scala', 'sbt' },
         callback = function()
-          require('metals').initialize_or_attach(
-            require('metals').bare_config()
-          )
+          local metals = require('metals')
+          local dap_ok, dap = pcall(require, 'dap')
+          local config = metals.bare_config()
+
+          if dap_ok then
+            config.on_attach = function()
+              metals.setup_dap()
+            end
+            dap.configurations.scala = {
+              {
+                type = 'scala',
+                request = 'launch',
+                name = 'Run or Test Target',
+                metals = {
+                  runType = 'runOrTestFile',
+                },
+              },
+              {
+                type = 'scala',
+                request = 'launch',
+                name = 'Test Target',
+                metals = {
+                  runType = 'testTarget',
+                },
+              },
+              {
+                type = 'scala',
+                request = 'attach',
+                name = 'Attach to Localhost',
+                hostName = 'localhost',
+                port = 5005,
+                buildTarget = 'root',
+              },
+            }
+          end
+
+          metals.initialize_or_attach(config)
         end,
       })
     end,
