903338954686c87113e46e8d3b34eddbc788eb4b
bekaboo <kankefengjing@gmail.com>
2025-10-19 22:04:20 -0400

    refactor(nvim): clean up `utils.snip.snips`

--- .config/nvim/lua/utils/snip/snips.lua
+++ .config/nvim/lua/utils/snip/snips.lua
@@ -90,7 +90,7 @@ local function snip_attr_add_new_opt(snip_attr, opt_key, opt_val)
   snip_attr[opt_key] = opt_val
 end
 
-local SNIPS = setmetatable({}, {
+return setmetatable({}, {
   __index = function(self, snip_name)
     local snip_attr_str = snip_name:gsub('^m?s', '')
     local snip_attr = {}
@@ -112,5 +112,3 @@ local SNIPS = setmetatable({}, {
     return self[snip_name]
   end,
 })
-
-return SNIPS
