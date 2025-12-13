5633f01be1d314ee0d10cff7d28617a2d8999004
bekaboo <kankefengjing@gmail.com>
2025-12-12 21:35:35 -0500

    feat(nvim): add new keymap `<Leader>U` to update plugins

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -491,6 +491,8 @@ require('utils.load').on_events(
     map('n', '<Leader>u', '<Cmd>packadd nvim.undotree|Undotree<CR>', {
       desc = 'Toggle undotree',
     })
+
+    map('n', '<Leader>U', vim.pack.update, { desc = 'Update plugins' })
   end)
 )
 
