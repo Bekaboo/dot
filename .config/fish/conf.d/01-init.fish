b795e4b17b1190f64a1a8795bfee333e732e54a9
bekaboo <kankefengjing@gmail.com>
2026-01-22 23:36:48 -0800

    feat(fish): restore old behavior of `alt-backspace`

--- .config/fish/conf.d/01-init.fish
+++ .config/fish/conf.d/01-init.fish
@@ -1,7 +1,14 @@
+if not status is-interactive
+    exit
+end
+
 # Disable venv prompt provided by `activate.fish` -- we handle it ourselves in
 # `functions/fish_right_prompt.fish`
 # Also, shouldn't export this variable with `set -gx` as we don't want to to
 # affect other shells, e.g. `bash` that is nested in `fish`
-if status is-interactive
-    set -g VIRTUAL_ENV_DISABLE_PROMPT true
-end
+set -g VIRTUAL_ENV_DISABLE_PROMPT true
+
+# Restore old behavior of alt-backspace (delete a word instead of token), see:
+# https://github.com/fish-shell/fish-shell/issues/10926
+# https://github.com/fish-shell/fish-shell/pull/10766
+bind alt-backspace backward-kill-word
