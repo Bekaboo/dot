f8178d2e46c11d98469aab597a186ac723855c70
bekaboo <kankefengjing@gmail.com>
2026-02-24 15:02:15 -0800

    fix(nvim): math snippets not working in markdown

--- .config/nvim/lua/my/pack/res/luasnip/snippets/markdown.lua
+++ .config/nvim/lua/my/pack/res/luasnip/snippets/markdown.lua
@@ -8,7 +8,7 @@ local t = ls.text_node
 local i = ls.insert_node
 local d = ls.dynamic_node
 
-M.math = require('my.pack.res.luasnip.snippets.tex').mathathathathath
+M.math = require('my.pack.res.luasnip.snippets.tex').math
 
 M.snippets = {
   us.mssn({
