a52437014fc02c8118dd87687e8d115e554ce4d1
bekaboo <kankefengjing@gmail.com>
2026-06-06 17:55:16 +0800

    feat(bin): add script `trans` to translate using openai-compatible api

---
+++ .config/fish/completions/trans.fish
@@ -0,0 +1,7 @@
+complete -c trans -f
+complete -c trans -s b -l base -d 'API base URL'
+complete -c trans -s k -l key -d 'API key'
+complete -c trans -s m -l model -d 'Model name'
+complete -c trans -s t -l to -d 'Target language'
+complete -c trans -s s -l stream -d 'Stream the response'
+complete -c trans -s h -l help -d 'Show help'
