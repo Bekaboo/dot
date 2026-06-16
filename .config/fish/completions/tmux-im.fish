b9490abae743d2e4d790f7b2f2615091b85c3b5a
bekaboo <kankefengjing@gmail.com>
2026-06-17 00:01:43 +0800

    refactor(bin,tmux)!: remove `tmux-im`'s `clear-status` subcommand (not used)
    Problem: When a pane exited, we don't need to manually clear `@im_status`
    	flag for the exited pane, tmux should do this for us.
    
    Fix:    Just turn off input method when a pane exists and let the
    	pane-focus-in hook in the new pane decide the input method
    	status.

--- .config/fish/completions/tmux-im.fish
+++ .config/fish/completions/tmux-im.fish
@@ -3,6 +3,5 @@ complete -c tmux-im -f
 complete -c tmux-im -n __fish_use_subcommand -a save -d 'Save IM state for a pane'
 complete -c tmux-im -n __fish_use_subcommand -a on -d 'Restore IM for a pane'
 complete -c tmux-im -n __fish_use_subcommand -a force-off -d 'Force IM off'
-complete -c tmux-im -n __fish_use_subcommand -a clear-status -d 'Clear IM status for a pane'
 
-complete -c tmux-im -n '__fish_seen_subcommand_from save on clear-status' -a '(tmux list-panes -a -F "#{pane_id}" 2>/dev/null)' -d 'Pane ID'
+complete -c tmux-im -n '__fish_seen_subcommand_from save on' -a '(tmux list-panes -a -F "#{pane_id}" 2>/dev/null)' -d 'Pane ID'
