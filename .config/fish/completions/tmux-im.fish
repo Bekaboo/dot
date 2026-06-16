ef17c0bbcc0a5795c03da40dd250b5f592451e4c
bekaboo <kankefengjing@gmail.com>
2026-06-17 00:11:11 +0800

    refactor(bin,tmux): rename `tmux-im`'s subcommand `force-off` to `off`

--- .config/fish/completions/tmux-im.fish
+++ .config/fish/completions/tmux-im.fish
@@ -2,6 +2,6 @@ complete -c tmux-im -f
 
 complete -c tmux-im -n __fish_use_subcommand -a save -d 'Save IM state for a pane'
 complete -c tmux-im -n __fish_use_subcommand -a on -d 'Restore IM for a pane'
-complete -c tmux-im -n __fish_use_subcommand -a force-off -d 'Force IM off'
+complete -c tmux-im -n __fish_use_subcommand -a off -d 'Turn IM off'
 
 complete -c tmux-im -n '__fish_seen_subcommand_from save on' -a '(tmux list-panes -a -F "#{pane_id}" 2>/dev/null)' -d 'Pane ID'
