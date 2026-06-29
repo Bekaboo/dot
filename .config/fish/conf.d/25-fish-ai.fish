5939f2d37dd73675c6cf296891eed011f079986f
bekaboo <kankefengjing@gmail.com>
2026-06-29 17:20:38 +0800

    feat(fish): link different fish-ai config depending on `$FISH_AI_CONFIG`

--- .config/fish/conf.d/25-fish-ai.fish
+++ .config/fish/conf.d/25-fish-ai.fish
@@ -4,3 +4,8 @@ end
 
 set -Ux FISH_AI_KEYMAP_1 ctrl-_ # convert comment to command or explain
 set -Ux FISH_AI_KEYMAP_2 alt-/ # autocomplete or fix previous command
+
+# Switch fish-ai config based on FISH_AI_CONFIG envvar
+if test -n "$FISH_AI_CONFIG"
+    ln -s $HOME/.config/fish-ai.$FISH_AI_CONFIG.ini $HOME/.config/fish-ai.ini
+end
