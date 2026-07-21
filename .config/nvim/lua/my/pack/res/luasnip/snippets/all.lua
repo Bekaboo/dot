d602a9d905e92a5fe6170b7585336191c94db244
bekaboo <kankefengjing@gmail.com>
2026-07-21 11:12:20 +0800

    feat(nvim): add varients for 'now' snippet

--- .config/nvim/lua/my/pack/res/luasnip/snippets/all.lua
+++ .config/nvim/lua/my/pack/res/luasnip/snippets/all.lua
@@ -24,6 +24,8 @@ M.snippets = {
     c(1, {
       i(nil, os.date()),
       i(nil, os.date('%Y-%m-%d')), -- ISO
+      i(nil, os.date('%Y%m%d')),
+      i(nil, os.date("%Y%m%d_%H%M%S")),
       i(nil, os.date('%m.%d.%Y')),
     })
   ),
